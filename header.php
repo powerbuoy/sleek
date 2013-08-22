119c06b6ce9725e92f511aac3c52486c8d9f63ed
Andreas Lagerkvist <powerbuoy@gmail.com>
2013-08-22 06:16:24 +0200

    No img in header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <header id="header">
 
-	<h1><a href="<?php echo home_url('/') ?>"><img src="<?php echo get_template_directory_uri() ?>/css/gfx/logo.png" alt="<?php bloginfo('name') ?>"></a></h1>
+	<h1><a href="<?php echo home_url('/') ?>"><?php bloginfo('name') ?></a></h1>
 
 	<p><?php bloginfo('description') ?></p>
 

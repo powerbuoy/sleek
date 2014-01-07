7bb3a68fd17448b5c59f13b27ac368c171f13050
Andreas Lagerkvist <powerbuoy@gmail.com>
2014-01-07 02:48:30 +0100

    Excerpt for Pages + some extra user fields

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -1,7 +1,38 @@
 <?php
+# Give pages excerpts
+# add_action('init', 'h5b_add_excerpts_to_pages');
+
+function h5b_add_excerpts_to_pages () {
+	add_post_type_support('page', 'excerpt');
+}
+
+# Add some fields to users
+# add_filter('user_contactmethods', 'h5b_add_user_fields');
+
+function h5b_add_user_fields ($fields) {
+	$fields['profession'] = __('Profession', 'h5b');
+
+	return $fields;
+}
+
+# Allow HTML in Widget Titles (with [tags])
+add_filter('widget_title', 'h5b_html_in_widget_titles');
+
+function h5b_html_in_widget_titles ($title)
+{
+	$title = str_replace('[', '<', $title);
+	$title = str_replace(']', '>', $title);
+	$title = strip_tags($title, '<a><blink><br><span>');
+
+	return $title;
+}
+
+# Cleanup HEAD
 # From: http://www.themelab.com/2010/07/11/remove-code-wordpress-header/
-# Comment if needed
+add_action('init', 'h5b_cleanup');
+
 function h5b_cleanup () {
+	# Comment if needed
 	## Really Simple Discovery
 	remove_action('wp_head', 'rsd_link');
 
@@ -16,17 +47,3 @@ function h5b_cleanup () {
 	remove_action('wp_head', 'index_rel_link');
 	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 }
-
-add_action('init', 'h5b_cleanup');
-
-# Allow HTML in Widget Titles (with [tags])
-function h5b_html_in_widget_titles ($title)
-{
-	$title = str_replace('[', '<', $title);
-	$title = str_replace(']', '>', $title);
-	$title = strip_tags($title, '<a><blink><br><span>');
-
-	return $title;
-}
-
-add_filter('widget_title', 'h5b_html_in_widget_titles');

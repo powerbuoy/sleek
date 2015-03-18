d193274b7f7bacc21e4437ecc7e5284f3b972d36
AL <powerbuoy@gmail.com>
2015-03-18 19:34:21 +0100

    Better post author + posts intro and sub nav now use new functions

--- modules/post-author.php
+++ modules/post-author.php
@@ -3,27 +3,43 @@
 		$usr = get_user_by('slug', get_query_var('author_name'));
 	}
 	else {
-		$usr = get_userdata(get_query_var('author'));
+		$usr = get_user_by('id', get_query_var('author'));
+	}
+
+	if (!$usr) {
+		$usr = get_user_by('id', get_the_author_meta('ID'));
 	}
 ?>
 
-<section id="post-author">
+<?php if ($usr) : ?>
+	<section id="post-author">
+		
+		<h2>
+			<?php echo get_avatar($usr->ID) ?> 
+			<?php echo $usr->display_name ?>
+		</h2>
 
-	<h1>
-		<?php echo get_avatar($usr->ID) ?> 
-		<?php echo $usr->display_name ?>
-	</h1>
-
-	<p><?php echo $usr->description ?></p>
-
-	<nav>
-		<a href="mailto:<?php echo $usr->user_email ?>"><?php echo $usr->user_email ?></a> 
-		<?php if ($usr->user_url) : ?>
-			<a href="<?php echo $usr->user_url ?>" target="_blank"><?php echo $usr->user_url ?></a> 
+		<?php if ($usr->description) : ?>
+			<p><?php echo $usr->description ?></p>
 		<?php endif ?>
-		<?php if (get_the_author_meta('googleplus', $usr->ID)) : ?>
-			<a href="<?php echo get_the_author_meta('googleplus', $usr->ID) ?>" target="_blank"><?php echo get_the_author_meta('googleplus', $usr->ID) ?></a>
-		<?php endif ?>
-	</nav>
 
-</section>
+		<p>
+			<?php if ($tel = get_user_meta($usr->ID, 'tel', true)) : ?>
+				<a href="tel:<?php echo $tel ?>"><?php echo $tel ?></a>
+			<?php endif ?>
+
+			<a href="mailto:<?php echo $usr->user_email ?>"><?php echo $usr->user_email ?></a>
+		</p>
+
+		<?php /* <nav>
+			<ul>
+				<?php foreach (array('twitter', 'facebok', 'linkedin', 'googleplus', 'skype') as $sn) : ?>
+					<?php if ($snlink = get_the_author_meta($sn, $usr->ID)) : ?>
+						<li><a href="<?php echo $snlink ?>"><?php echo ucfirst($sn) ?></a></li>
+					<?php endif ?>
+				<?php endforeach ?>
+			</ul>
+		</nav> */ ?>
+
+	</section>
+<?php endif ?>
--- modules/sub-nav.php
+++ modules/sub-nav.php
@@ -1,33 +1,16 @@
 <?php
 	global $post;
 
-	if (is_page($post)) {
-		if ($post->post_parent) {
-			$parent = get_page($post->post_parent);
-
-			while ($parent->post_parent) {
-				$parent = get_page($parent->post_parent);
-			}
-
-			$children = wp_list_pages('title_li=&child_of=' . $parent->ID . '&echo=0&link_before=&link_after=');
-			$title = $parent->post_title;
-			$url = get_permalink($parent->ID);
-		}
-		else {
-			$children = wp_list_pages('title_li=&child_of=' . $post->ID . '&echo=0&link_before=&link_after=');
-			$title = $post->post_title;
-			$url = get_permalink($post->ID);
-		}
-	}
+	$sub_nav_tree = h5b_get_sub_nav_tree($post);
 ?>
 
-<?php if ($children) : ?>
+<?php if ($sub_nav_tree['children']) : ?>
 	<nav id="sub-nav">
 
-		<h2><a href="<?php echo $url ?>"><?php echo $title ?></a></h2>
+		<h2><a href="<?php echo $sub_nav_tree['url'] ?>"><?php echo $sub_nav_tree['title'] ?></a></h2>
 	
 		<ul>
-			<?php echo $children ?>
+			<?php echo $sub_nav_tree['children'] ?>
 		</ul>
 
 	</nav>

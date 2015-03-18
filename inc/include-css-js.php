2788548865a2a4b9a43fed494612755943a1e728
AL <powerbuoy@gmail.com>
2015-03-18 17:22:55 +0100

    Only include JS from child theme + number_formatJS

--- inc/include-css-js.php
+++ inc/include-css-js.php
@@ -18,8 +18,8 @@ function h5b_register_css_js () {
 #	wp_register_script('h5b_head', get_template_directory_uri() . '/js/head.php', array(), filemtime(TEMPLATEPATH . '/js/head.js'));
 #	wp_enqueue_script('h5b_head');
 
-	wp_register_script('h5b_foot', get_template_directory_uri() . '/js/foot.php', array(), filemtime(TEMPLATEPATH . '/js/foot.js'), true);
-	wp_enqueue_script('h5b_foot');
+#	wp_register_script('h5b_foot', get_template_directory_uri() . '/js/foot.php', array(), filemtime(TEMPLATEPATH . '/js/foot.js'), true);
+#	wp_enqueue_script('h5b_foot');
 
 	# Theme CSS
 #	wp_register_style('h5b', get_template_directory_uri() . '/css/all.css', array(), filemtime(TEMPLATEPATH . '/css/all.css'));

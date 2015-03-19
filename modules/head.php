7f21b228016f54a682d032ccff683a47e4a4e80b
AL <powerbuoy@gmail.com>
2015-03-19 15:10:31 +0100

    Mobile friendly

--- modules/head.php
+++ modules/head.php
@@ -17,6 +17,7 @@
 	<head>
 
 		<meta charset="<?php bloginfo('charset') ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<title><?php wp_title(' - ', true, 'right') ?></title>
 

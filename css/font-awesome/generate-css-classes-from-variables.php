73fd34bb7900d7579f1f28f29620160ba739f44f
AL <powerbuoy@gmail.com>
2015-03-19 20:19:08 +0100

    FontAwesome with new awesome classes and mixins + better better button

---
+++ css/font-awesome/generate-css-classes-from-variables.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+$vars = file('variables.scss');
+
+foreach ($vars as $var) {
+	preg_match('/\$fa\-var\-(.*?): (.*?);/', $var, $matches);
+	echo ".icon-{$matches[1]}:before, .icon-{$matches[1]}.after:after {content: {$matches[2]}}\n";
+}

4e1ad6032f6b73ea0a95a511bc812d3967ccb917
AL <powerbuoy@gmail.com>
2015-03-22 18:32:36 +0100

    Dont print ul.tags if there are no tags

--- modules/partials/tags.php
+++ modules/partials/tags.php
@@ -28,13 +28,14 @@
 	));
 ?>
 
-<ul class="tags">
-	<?php foreach ($theTags as $theTag) : ?>
-		<li>
-			<a href="<?php echo get_term_link($theTag) ?>" class="icon-<?php echo isset($tag2ico[$theTag->slug]) ? $tag2ico[$theTag->slug] : $theTag->slug ?>">
-				<?php echo $theTag->name ?>
-			</a>
-		</li>
-	<?php endforeach ?>
-</ul>
-
+<?php if ($theTags) : ?>
+	<ul class="tags">
+		<?php foreach ($theTags as $theTag) : ?>
+			<li>
+				<a href="<?php echo get_term_link($theTag) ?>" class="icon-<?php echo isset($tag2ico[$theTag->slug]) ? $tag2ico[$theTag->slug] : $theTag->slug ?>">
+					<?php echo $theTag->name ?>
+				</a>
+			</li>
+		<?php endforeach ?>
+	</ul>
+<?php endif ?>

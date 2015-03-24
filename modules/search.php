22cc1070d4f10b917f4e86afe9d73fac0d494abb
AL <powerbuoy@gmail.com>
2015-03-24 16:14:43 +0100

    input type search

--- modules/search.php
+++ modules/search.php
@@ -1,7 +1,7 @@
-<form method="get" action="<?php echo home_url() ?>">
+<form method="get" action="<?php echo home_url() ?>" class="search">
 
 	<p>
-		<input type="text" name="s" value="<?php echo trim(get_search_query()) ?>" placeholder="<?php _e('Search...', 'sleek') ?>"> 
+		<input type="search" name="s" value="<?php echo trim(get_search_query()) ?>" placeholder="<?php _e('Search...', 'sleek') ?>"> 
 		<input type="submit" value="<?php _e('Find', 'sleek') ?>">
 	</p>
 

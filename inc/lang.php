96fdf3b042dd897119d02f9f1a790c547dbde172
AL <powerbuoy@gmail.com>
2015-03-25 19:30:51 +0100

    Translations

--- inc/lang.php
+++ inc/lang.php
@@ -1,7 +1,12 @@
 <?php
 # Set up for translation
-# add_action('after_setup_theme', 'sleek_setup_lang');
+add_action('after_setup_theme', 'sleek_setup_lang');
 
 function sleek_setup_lang () {
-	load_theme_textdomain('sleek', get_stylesheet_directory() . '/languages');
+	global $l10n;
+
+	if (!load_theme_textdomain('sleek', get_template_directory() . '/lang')) {
+	#	var_dump($l10n);
+	#	die(get_template_directory() . '/lang');
+	}
 }

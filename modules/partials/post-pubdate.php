9d8164a05e96ddf47ac86c8d51e5c638a53e821d
AL <powerbuoy@gmail.com>
2015-03-27 18:46:30 +0100

    Refactoring

--- modules/partials/post-pubdate.php
+++ modules/partials/post-pubdate.php
@@ -1,4 +1,4 @@
-<p>
+<p class="pubdate">
 	<?php printf(__('Posted %s by %s', 'sleek'), 
 		'<time datetime="' . get_the_time('Y-m-j') . '">' . get_the_time(get_option('date_format')) . '</time>', 
 		'<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('display_name') . '</a>'

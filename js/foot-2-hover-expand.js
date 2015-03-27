f4ee0765920e9e2489d45b5f8c7e7d62a8b64691
AL <powerbuoy@gmail.com>
2015-03-27 16:15:54 +0100

    Plugins in sleek

--- js/foot-2-hover-expand.js
+++ js/foot-2-hover-expand.js
@@ -1,10 +1,10 @@
-var HoverCode = {
+var HoverExpand = {
 	init: function () {
 		var codes = document.querySelectorAll('pre code');
 
 		for (var i = 0; i < codes.length; i++) {
 			codes[i].parentNode.style.width = (codes[i].offsetWidth + 25) + 'px';
-			codes[i].parentNode.classList.add('hover-code');
+			codes[i].parentNode.classList.add('hover-expand');
 			codes[i].parentNode.classList.add('prettyprint');
 		}
 	}

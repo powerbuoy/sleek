26cc7527c41ef4eb35b87c3264d8c23a517b571d
AL <powerbuoy@gmail.com>
2015-03-28 21:44:42 +0100

    New shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -32,10 +32,38 @@ function sleek_shortcode_include_module ($atts) {
 	$include_path	= file_exists($include_path) ? $include_path : TEMPLATEPATH . $suffix;
 
 	if (!file_exists($include_path)) {
-		return '<p><strong>[ include error: Module "' . $atts['mod'] . '" does not exist ]</strong></p>';
+		return '<p><strong>[ Include Error: Module "' . $atts['mod'] . '" does not exist ]</strong></p>';
 	}
 
 	extract($atts);
 
 	return fetch($include_path, $atts);
+}
+
+# MarkdownFile
+# add_shortcode('markdown-file', 'sleek_shortcode_markdown_file');
+
+function sleek_shortcode_markdown_file ($atts) {
+	if (!isset($atts['file'])) {
+		return '<p><strong>[ Markdown-file error: Have to set file ]</strong></p>';
+	}
+
+	$include_path = WP_CONTENT_DIR . '/themes/' . $atts['file'];
+
+	if (!file_exists($include_path)) {
+		return '<p><strong>[ Markdown-file Error: File "' . $atts['file'] . '" does not exist ]</strong></p>';
+	}
+
+	$md = file_get_contents($include_path);
+
+	if (!file_exists(WP_CONTENT_DIR . '/plugins/wp-markdown/markdown-extra.php')) {
+		$md = "<p><strong>[ Markdown-file Error: Markdown() missing - please install wp-markdown plug-in (you don't even have to activate it) ]</strong></p>" . $md;
+	}
+	else {
+		require_once WP_CONTENT_DIR . '/plugins/wp-markdown/markdown-extra.php';
+
+		$md = Markdown($md);
+	}
+
+	return $md;
 }

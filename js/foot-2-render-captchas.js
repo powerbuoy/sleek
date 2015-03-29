c5eab82616ac1b920dd9da85ffc849e4067298af
AL <powerbuoy@gmail.com>
2015-03-29 16:51:55 +0200

    Rename + move to config

--- js/foot-2-render-captchas.js
+++ js/foot-2-render-captchas.js
@@ -1,9 +1,9 @@
-var Captchas = function () {
+var RenderCaptchas = function () {
 	var captchas = document.querySelectorAll('div.captcha');
 
 	for (var i = 0; i < captchas.length; i++) {
 		var widgetID = grecaptcha.render(captchas[i], {
-			sitekey: '6Ld0FQQTAAAAADAb-WQKUveGUHFP6IAYjuIWthBv' // TODO: Move to config
+			sitekey: RECAPTCHA_SITE_KEY
 		});
 
 		captchas[i].setAttribute('data-captcha-widget-id', widgetID);

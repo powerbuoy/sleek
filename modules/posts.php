2aa269c490bdd9751ce6e221815419579b80d277
AL <powerbuoy@gmail.com>
2015-03-29 20:32:30 +0200

    Dont pritn pagination if there is none

--- modules/posts.php
+++ modules/posts.php
@@ -33,7 +33,13 @@
 
 </section>
 
-<nav id="pagination">
-	<?php previous_posts_link(__('Previous page', 'sleek')) ?>
-	<?php next_posts_link(__('Next page', 'sleek')) ?>
-</nav>
+<?php
+	$prev = get_previous_posts_link(__('Previous page', 'sleek'));
+	$next = get_next_posts_link(__('Next page', 'sleek'));
+?>
+
+<?php if ($prev or $next) : ?>
+	<nav id="pagination">
+		<?php echo $prev . $next ?>
+	</nav>
+<?php endif ?>

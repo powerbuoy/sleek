7698d76d38d72cd3e563d9fea0020cedf73b0bc4
AL <powerbuoy@gmail.com>
2015-03-29 16:57:47 +0200

    Nicer email template

--- inc/html5form/template.php
+++ inc/html5form/template.php
@@ -1,15 +1,33 @@
+<?php $ignore = array('contact_submit', 'sleek_module', 'g-recaptcha-response', ''); ?>
+
 <!doctype html>
 
 <html>
 	<head>
 		<meta charset="utf8">
 		<title>From website</title>
+		<style>
+			table {
+				width: 100%;
+				border: 1px solid #eee;
+				border-collapse: collapse;
+				border-spacing: 0;
+			}
+
+			th, 
+			td {
+				text-align: center;
+				vertical-align: top;
+				padding: 5px 10px;
+				border: 1px solid #eee;
+			}
+		</style>
 	</head>
 	<body>
 		<table>
-			<?php foreach ($fields as $field => $value) : ?>
+			<?php foreach ($fields as $field => $value) : if (!in_array($ignore, $field)) : ?>
 				<tr>
-					<th scope="row"><?php echo $field ?></th>
+					<th scope="row"><?php echo ucfirst(str_replace('_', ' ', $field)) ?></th>
 					<td>
 						<?php if (is_array($value)) : ?>
 							<?php echo implode(', ', $value) ?>
@@ -18,7 +36,7 @@
 						<?php endif ?>
 					</td>
 				</tr>
-			<?php endforeach ?>
+			<?php endif; endforeach ?>
 		</table>
 	</body>
 </html>

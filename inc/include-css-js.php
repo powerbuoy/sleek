ba1f787d56b86293e9ce6cf109d2d7ff49c8f771
AL <powerbuoy@gmail.com>
2015-03-29 15:20:07 +0200

    Remove unused code

--- inc/include-css-js.php
+++ inc/include-css-js.php
@@ -1,33 +1,4 @@
 <?php
-# Theme CSS/JS
-# add_action('wp_enqueue_scripts', 'sleek_register_css_js');
-
-function sleek_register_css_js () {
-	# jQuery
-#	wp_deregister_script('jquery');
-#	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'); # 2.0.0
-
-#	wp_register_script('jquery_migrate', 'http://code.jquery.com/jquery-migrate-1.1.0.js', array('jquery'));
-#	wp_enqueue_script('jquery_migrate');
-
-	# Google Maps
-#	wp_register_script('google_maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false');
-#	wp_enqueue_script('google_maps');
-
-	# Theme Head/Foot JS
-#	wp_register_script('sleek_head', get_template_directory_uri() . '/js/head.php', array(), null);
-#	wp_register_script('sleek_head', get_template_directory_uri() . '/js/head.' . filemtime(get_template_directory() . '/js/head.js') . '.js', array(), null);
-#	wp_enqueue_script('sleek_head');
-
-#	wp_register_script('sleek_foot', get_template_directory_uri() . '/js/foot.php', array(), null, true);
-#	wp_register_script('sleek_foot', get_template_directory_uri() . '/js/foot.' . filemtime(get_template_directory() . '/js/head.js') . '.js', array(), null, true);
-#	wp_enqueue_script('sleek_foot');
-
-	# Theme CSS
-#	wp_register_style('sleek', get_template_directory_uri() . '/css/all.' . filemtime(get_template_directory() . '/css/all.css') . '.css', array(), null);
-#	wp_enqueue_style('sleek');
-}
-
 # Admin CSS/JS
 # add_action('admin_enqueue_scripts', 'sleek_register_css_js_admin');
 

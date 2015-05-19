ec153d2ad69c0bfb51d951afc239e3c88301dad4
AL <powerbuoy@gmail.com>
2015-05-19 04:13:39 +0200

    Allow HTML in Site Title and Description (but dont print it in <title>)

--- modules/head.php
+++ modules/head.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 
 <?php
-	if (is_front_page())	$htmlID = 'home-page';
-	elseif (is_single())	$htmlID = 'post-page';
-	elseif (is_archive())	$htmlID = 'archive-page';
-	elseif (is_page())		$htmlID = 'page-page';
-	elseif (is_search())	$htmlID = 'search-page';
-	else					$htmlID = 'unknown-page';
+	if (is_front_page())	$htmlID = 'home';
+	elseif (is_single())	$htmlID = 'post';
+	elseif (is_archive())	$htmlID = 'archive';
+	elseif (is_page())		$htmlID = 'page';
+	elseif (is_search())	$htmlID = 'search';
+	else					$htmlID = 'unknown';
 ?>
 
-<html id="<?php echo $htmlID ?>" <?php language_attributes() ?> <?php body_class('no-js') ?>>
+<html id="<?php echo $htmlID ?>-page" <?php language_attributes() ?> <?php body_class('no-js') ?>>
 	
 	<head>
 
 		<meta charset="<?php bloginfo('charset') ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<title><?php wp_title(' - ', true, 'right') ?></title>
+		<?php /* <title><?php echo wp_title(' - ', true, 'right') ?></title> */ ?>
+		<title><?php echo strip_tags(str_replace(array('&lt;', '&gt;'), array('<', '>'), wp_title(' - ', false, 'right'))) ?></title>
 
 		<?php wp_head() ?>
 
--- modules/header.php
+++ modules/header.php
@@ -1,12 +1,17 @@
+<?php
+	$title = str_replace(array('&lt;', '&gt;'), array('<', '>'), get_bloginfo('name'));
+	$tagline = str_replace(array('&lt;', '&gt;'), array('<', '>'), get_bloginfo('description'));
+?>
+
 <header id="header">
 
 	<?php if (is_front_page()) : ?>
-		<h1><a href="<?php echo home_url('/') ?>"><?php bloginfo('name') ?></a></h1>
+		<h1><a href="<?php echo home_url('/') ?>"><?php echo $title ?></a></h1>
 	<?php else : ?>
-		<p class="logo"><a href="<?php echo home_url('/') ?>"><?php bloginfo('name') ?></a></p>
+		<p class="logo"><a href="<?php echo home_url('/') ?>"><?php echo $title ?></a></p>
 	<?php endif ?>
 
-	<?php if ($tagline = get_bloginfo('description')) : ?>
+	<?php if ($tagline) : ?>
 		<p class="tagline"><?php echo $tagline ?></p>
 	<?php endif ?>
 

da3be54ae2079702e0a5116b64a6dd027f2ddfe9
AL <powerbuoy@gmail.com>
2015-05-19 03:05:09 +0200

    Only print the tagline if there is one

--- modules/header.php
+++ modules/header.php
@@ -6,7 +6,9 @@
 		<p class="logo"><a href="<?php echo home_url('/') ?>"><?php bloginfo('name') ?></a></p>
 	<?php endif ?>
 
-	<p class="tagline"><?php bloginfo('description') ?></p>
+	<?php if ($tagline = get_bloginfo('description')) : ?>
+		<p class="tagline"><?php echo $tagline ?></p>
+	<?php endif ?>
 
 	<?php dynamic_sidebar('header') ?>
 

ac4164dbf1840dda2887025669354097ed6be9cd
AL <powerbuoy@gmail.com>
2016-03-15 02:01:57 +0100

    Loads of stuff happening

--- header.php
+++ header.php
@@ -1,10 +1,5 @@
 <?php
-	$module = isset($_GET['sleek_module']) ? 
-				$_GET['sleek_module'] : 
-				(isset($_POST['sleek_module']) ? 
-					$_POST['sleek_module'] : 
-					false
-				);
+	$module = isset($_GET['sleek_module']) ? $_GET['sleek_module'] : (isset($_POST['sleek_module']) ? $_POST['sleek_module'] : false);
 
 	if (XHR and $module) {
 		sleek_get_module(basename($module));

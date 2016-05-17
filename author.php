3a023a1928082d77f2d84ee926ca6577bf18db22
AL <powerbuoy@gmail.com>
2016-05-17 11:40:01 +0200

    Added mandatory sidebar.php

--- author.php
+++ author.php
@@ -7,12 +7,5 @@
 
 </main>
 
-<?php if (is_active_sidebar('aside')) : ?>
-	<aside id="aside">
-
-		<?php dynamic_sidebar('aside') ?>
-
-	</aside>
-<?php endif ?>
-
+<?php get_sidebar() ?>
 <?php get_footer() ?>
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<?php if (is_active_sidebar('aside')) : ?>
+	<aside id="aside">
+
+		<?php dynamic_sidebar('aside') ?>
+
+	</aside>
+<?php endif ?>

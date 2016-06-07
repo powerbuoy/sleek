cf0495d3bacd317dd6b23b42f1b821b8a4dea703
AL <powerbuoy@gmail.com>
2016-06-07 14:13:44 +0200

    Much better posts pagination :D

--- modules/posts-pagination.php
+++ modules/posts-pagination.php
@@ -1,10 +1,36 @@
 <?php
-	$prev = get_previous_posts_link(__('Previous page', 'sleek'));
-	$next = get_next_posts_link(__('Next page', 'sleek'));
+	$big = 999999;
+	$pagination = paginate_links(array(
+		# All args
+		/* 'base' => '%_%',
+		'format' => '/page/%#%',
+		'total' => 1,
+		'current' => 0,
+		'show_all' => false,
+		'end_size' => 1,
+		'mid_size' => 2,
+		'prev_next' => true,
+		'prev_text' => __('« Previous'),
+		'next_text' => __('Next »'),
+		'type' => 'array',
+		'add_args' => false,
+		'add_fragment' => '',
+		'before_page_number' => '',
+		'after_page_number' => '', */
+
+		# From https://codex.wordpress.org/Function_Reference/paginate_links
+		'type' => 'list',
+		'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+		'format' => '?paged=%#%',
+		'current' => max(1, get_query_var('paged')),
+		'total' => $wp_query->max_num_pages
+	));
 ?>
 
-<?php if ($prev or $next) : ?>
+<?php if ($pagination) : ?>
 	<nav id="pagination">
-		<?php echo $prev . $next ?>
+
+		<?php echo $pagination ?>
+
 	</nav>
 <?php endif ?>

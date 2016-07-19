a8efa4ec59300d19b55c6022530ced1efffe7367
AL <powerbuoy@gmail.com>
2016-07-19 14:26:20 +0200

    Better register post type/taxonomies

--- inc/helpers/register-post-types.php
+++ inc/helpers/register-post-types.php
@@ -2,48 +2,142 @@
 /**
  * Register custom post types
  *
- * Takes $postTypes as 'slug' => 'Name of sidebar' or optional config array.
- * Pass in an optional text domain as the second argument (for translating URLs etc)
+ * Pass in an array of custom post type slugs. You can optionally override the default
+ * configuration by passing in a multi-dimensional array; array('my_post_type' => ['my-config' => 'my-value'])
  */
 function sleek_register_post_types ($postTypes, $textdomain = 'sleek') {
-	# Register Post Types
-	foreach ($postTypes as $postType => $description) {
-		$slug = $textdomain ? __('url_' . $postType, $textdomain) : $postType;
+	foreach ($postTypes as $postType => $data) {
+		# If no data was supplied - a one-dimensional array is assumed
+		if (!is_array($data)) {
+			$postType = $data;
+		}
 
+		# Create the post type slug - if a textdomain is specified make it translatable, otherwise make it dash-separated
+		$slug = $textdomain ? __('url_' . $postType, $textdomain) : str_replace('_', '-', $postType);
+
+		# Create the post type nice-name based on the the postType name
+		$name = __(ucfirst(str_replace('_', ' ', $postType)), $textdomain);
+
+		# Create the config
 		$config = array(
-			'labels'				=> array(
-				'name'					=> __(ucfirst(str_replace('_', ' ', $postType)), $textdomain),
-				'singular_label'		=> __(ucfirst(str_replace('_', ' ', $postType)), $textdomain)
+			'labels' => array(
+				'name' => $name,
+				'singular_label' => $name
 			),
-			'rewrite'				=> array(
-				'with_front'			=> false,
-				'slug'					=> $slug
+			'rewrite' => array(
+				'with_front' => false,
+				'slug' => $slug
 			),
-			'exclude_from_search'	=> false,
-			'has_archive'			=> true,
-			'public'				=> true,
-			'supports'				=> array(
-				'title',
-				'editor',
-				'author',
-				'thumbnail',
-				'excerpt',
-				'wpcom-markdown',
-				'trackbacks',
-				'custom-fields',
-				'revisions',
-				'page-attributes',
-				'comments'
+			'exclude_from_search' => false,
+			'has_archive' => true,
+			'public' => true,
+			'supports' => array(
+				'title', 'editor', 'author', 'thumbnail', 'excerpt',
+				'wpcom-markdown', 'trackbacks', 'custom-fields',
+				'revisions', 'page-attributes', 'comments'
 			)
 		);
 
-		if (is_array($description)) {
-			$config = array_merge($config, $description);
-		}
-		else {
-			$config['description'] = $description;
+		# If a config array was specified
+		if (is_array($data)) {
+			$config = array_merge($config, $data);
 		}
 
 		register_post_type($postType, $config);
+	}
+}
+
+function sleek_register_post_type_meta_data ($postTypes, $textdomain = 'sleek') {
+	add_action('admin_print_styles', function () {
+		echo '<style>
+			div.sleek-cpt-meta-data {
+			}
+
+			div.sleek-cpt-meta-data form {
+				padding-top: 10px;
+			}
+
+			div.sleek-cpt-meta-data div.form-field {
+				margin: 0 0 2rem;
+			}
+
+			/* Copied from WPs normal title field... */
+			div.sleek-cpt-meta-data div.form-field.title input {
+				padding: 3px 8px;
+				font-size: 1.7em;
+				line-height: 100%;
+				height: 1.7em;
+				width: 100%;
+				outline: 0;
+				margin: 0 0 3px;
+				background-color: #fff;
+			}
+		</style>';
+	});
+
+	foreach ($postTypes as $postType => $data) {
+		# If no data was supplied - a one-dimensional array is assumed
+		if (!is_array($data)) {
+			$postType = $data;
+		}
+
+		# Create the nice name
+		$name = __(ucfirst(str_replace('_', ' ', $postType)), $textdomain);
+
+		# Add the admin menu
+		add_submenu_page(
+			# Parent slug
+			'edit.php?post_type=' . $postType,
+
+			# Page title
+			sprintf(__('%s Meta Data', $textdomain), $name),
+
+			# Menu title
+			sprintf(__('%s Meta Data', $textdomain), $name),
+
+			# Capability needed
+			'manage_options',
+
+			# Page slug
+			'edit-' . $postType . '-meta',
+
+			# The function that adds the settings screen
+			function () use ($postType, $textdomain, $name) {
+				?>
+				<div class="wrap sleek-cpt-meta-data">
+
+					<h1><?php printf(__('Edit %s Meta Data', $textdomain), $name) ?></h1>
+
+					<form method="post" action="options.php">
+
+						<div class="form-field title">
+							<input type="text"
+								name="<?php echo $postType ?>_title"
+								value="<?php echo stripslashes(get_option($postType . '_title')) ?>"
+								placeholder="<?php _e('Title', $textdomain) ?>">
+						</div>
+
+						<?php wp_editor(
+							stripslashes(get_option($postType . '_description')),
+							$postType . '_settings',
+							array('textarea_name' => $postType . '_description')
+						) ?>
+
+						<?php settings_fields($postType . '_settings') ?>
+						<?php submit_button() ?>
+
+					</form>
+
+				</div>
+				<?php
+			}
+		);
+
+		# Add our new options
+		add_action('admin_init', function () use ($postType, $textdomain, $name) {
+			register_setting($postType . '_settings', $postType . '_title');
+			register_setting($postType . '_settings', $postType . '_description');
+			register_setting($postType . '_settings', $postType . '_image', 'intval');
+		});
 	}
 }
--- inc/helpers/register-taxonomies.php
+++ inc/helpers/register-taxonomies.php
@@ -7,23 +7,28 @@
  */
 function sleek_register_taxonomies ($taxonomies, $textdomain = 'sleek') {
 	foreach ($taxonomies as $taxonomy => $forPostTypes) {
-		$slug = $textdomain ? __('url_' . $taxonomy, $textdomain) : $taxonomy;
+		# Create the post type slug - if a textdomain is specified make it translatable, otherwise make it dash-separated
+		$slug = $textdomain ? __('url_' . $taxonomy, $textdomain) : str_replace('_', '-', $taxonomy);
+
+		# Create the post type nice-name based on the the postType name
+		$name = __(ucfirst(str_replace('_', ' ', $taxonomy)), $textdomain);
 
 		# Assume it should be treated as a tag (non hierarchical) if the slug has "tag" in the name
+		# TODO: Is this a shit assumtion?? :P
 		$hierarchical = strpos($taxonomy, 'tag') !== false ? false : true;
-		# $hierarchical = true;
 
+		# Register the taxonomy
 		register_taxonomy($taxonomy, $forPostTypes, array(
-			'labels'			=> array(
-				'name'			=> __(ucfirst(str_replace('_', ' ', $taxonomy)), $textdomain),
-				'singular_label'=> __(ucfirst(str_replace('_', ' ', $taxonomy)), $textdomain)
+			'labels' => array(
+				'name' => $name,
+				'singular_label' => $name
 			),
-			'rewrite'			=> array(
-				'with_front'	=> false,
-				'slug'			=> $slug
+			'rewrite' => array(
+				'with_front' => false,
+				'slug' => $slug
 			),
-			'sort'				=> true,
-			'hierarchical'		=> $hierarchical
+			'sort' => true,
+			'hierarchical' => $hierarchical
 		));
 	}
 }

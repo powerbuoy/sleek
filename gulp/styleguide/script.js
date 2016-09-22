200e1c6aa2e9180e1651210ec8a9766955537d34
AL <powerbuoy@gmail.com>
2016-09-22 03:10:05 +0200

    Much nicer styleguide

--- gulp/styleguide/script.js
+++ gulp/styleguide/script.js
@@ -1,16 +1,16 @@
 // Insert all example code into the page so we can actually see the examples
 //window.addEventListener('load', function () {
-    var elements = document.querySelectorAll('.styleguide__element');
+	var elements = document.querySelectorAll('section article');
 
-    for (var i = 0; i < elements.length; i++) {
-        (function () {
-            var element = elements[i];
-            var exampleCode = element.querySelector('.styleguide__example-code');
-            var example = element.querySelector('.styleguide__example');
+	for (var i = 0; i < elements.length; i++) {
+		(function () {
+			var element = elements[i];
+			var exampleCode = element.querySelector('.example-code');
+			var example = element.querySelector('.example');
 
-            if (exampleCode) {
-                example.innerHTML = exampleCode.textContent;
-            }
-        })();
-    }
+			if (exampleCode) {
+				example.innerHTML = exampleCode.textContent;
+			}
+		})();
+	}
 //});

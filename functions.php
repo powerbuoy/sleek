6dea11d4a45fe24298c4e3143d83458486f54a34
AL <powerbuoy@gmail.com>
2016-09-27 21:42:10 +0200

    More cleanup

--- functions.php
+++ functions.php
@@ -4,13 +4,11 @@ include get_template_directory() . '/inc/roots/utils.php';
 include get_template_directory() . '/inc/roots/cleanup.php';
 include get_template_directory() . '/inc/roots/nav.php';
 
-# This is needed by include and markdown-file, move this down when shortcodes are moved to plugins
+# Sleek stuff
 include get_template_directory() . '/inc/utils.php';
 
-# Shortcodes (TODO: Move to separate plugins)
-include get_template_directory() . '/inc/shortcodes/markdown-file.php';
+include get_template_directory() . '/inc/shortcodes/markdown-file.php'; # TODO: Move to plugin
 
-# WP fixes and hacks
 include get_template_directory() . '/inc/active-archive-link-on-taxonomies.php';
 include get_template_directory() . '/inc/add-post-type-arg-to-get-terms.php';
 include get_template_directory() . '/inc/attachments-archive.php';
@@ -25,10 +23,10 @@ include get_template_directory() . '/inc/hide-sleek-from-admin.php';
 include get_template_directory() . '/inc/html-in-widget-titles.php';
 include get_template_directory() . '/inc/jquery-cdn-in-foot.php';
 include get_template_directory() . '/inc/more-markdown.php';
-include get_template_directory() . '/inc/open-graph-tags.php';
 include get_template_directory() . '/inc/pagination-css-class.php';
 
 include get_template_directory() . '/inc/post-thumbnails.php';
+include get_template_directory() . '/inc/reduce-requests.php';
 
 include get_template_directory() . '/inc/register-acf.php';
 include get_template_directory() . '/inc/register-assets.php';
@@ -37,18 +35,12 @@ include get_template_directory() . '/inc/register-sidebars.php';
 include get_template_directory() . '/inc/register-taxonomies.php';
 include get_template_directory() . '/inc/register-theme-options.php';
 
-include get_template_directory() . '/inc/disable-wp-embed.php';
-include get_template_directory() . '/inc/remove-emoji-css-js.php';
-include get_template_directory() . '/inc/remove-home-from-yoast-breadcrumbs.php';
-
-include get_template_directory() . '/inc/exclude-current-post-in-upw.php';
-
 include get_template_directory() . '/inc/set-cpt-in-search.php';
 include get_template_directory() . '/inc/setup-lang.php';
-
 include get_template_directory() . '/inc/show-all-post-types-for-auhors.php';
 include get_template_directory() . '/inc/unset-active-blog-class.php';
 
+# Currently unused
 # include get_template_directory() . '/inc/ajax.php';
 # include get_template_directory() . '/inc/relevanssi.php';
 # include get_template_directory() . '/inc/tinymce-styles.php';
---
+++ inc/reduce-requests.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Reduces the number of HTTP requests by removing
+ * unwanted CSS and JS files from WP and plug-ins
+ */
+function sleek_reduce_requests ($except = []) {
+	# WP Embed (URL missing)
+	if (!in_array('wp_oembed', $except)) {
+		# Remove the REST API endpoint.
+		remove_action('rest_api_init', 'wp_oembed_register_route');
+
+		# Turn off oEmbed auto discovery. Don't filter oEmbed results.
+		remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+
+		# Remove oEmbed discovery links.
+		remove_action('wp_head', 'wp_oembed_add_discovery_links');
+
+		# Remove oEmbed-specific JavaScript from the front-end and back-end.
+		remove_action('wp_head', 'wp_oembed_add_host_js');
+	}
+
+	# Emoji
+	if (!in_array('wp_emoji', $except)) {
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_print_styles', 'print_emoji_styles');
+	}
+
+	# CF7
+	if (!in_array('wpcf7_css', $except)) {
+		add_filter('wpcf7_load_css', '__return_false');
+	}
+	if (!in_array('wpcf7_js', $except)) {
+		add_filter('wpcf7_load_js', '__return_false');
+	}
+
+	# UPW
+	if (!in_array('upw', $except)) {
+		add_filter('upw_enqueue_styles', '__return_false');
+	}
+
+	# WPMU Signup Stylesheet
+	if (!in_array('wpmu_signup_stylesheet', $except)) {
+		add_action('get_header', function () {
+			remove_action('wp_head', 'wpmu_signup_stylesheet');
+		});
+	}
+}

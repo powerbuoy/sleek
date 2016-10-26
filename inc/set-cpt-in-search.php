86b1b234145b88fd27227696404d72f299fb855f
AL <powerbuoy@gmail.com>
2016-10-26 03:37:35 +0200

    WTF :P

--- inc/set-cpt-in-search.php
+++ inc/set-cpt-in-search.php
@@ -6,11 +6,16 @@
  * so that custom taxonomy archives still work (WP Bug https://core.trac.wordpress.org/ticket/20234)
  * We need to adjust which should _actually_ show up ín search using pre_get_posts
  */
-function sleek_set_cpt_in_search ($pts = []) {
-	$postTypes = array_merge(['post', 'page'], $pts);
+function sleek_set_cpt_in_search ($pts = [], $override = false) {
+	if ($override) {
+		$postTypes = $pts;
+	}
+	else {
+		$postTypes = array_merge(['post', 'page'], $pts);
+	}
 
-	add_filter('pre_get_posts', function ($query) {
-		if ($query->is_search and !$query->is_admin and $query->is_main_query) {
+	add_filter('pre_get_posts', function ($query) use ($postTypes) {
+		if ($query->is_search() and !$query->is_admin() and $query->is_main_query()) {
 			if (isset($_GET['post_type'])) {
 				$query->set('post_type', [$_GET['post_type']]);
 			}

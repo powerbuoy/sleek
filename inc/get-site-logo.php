f516dea21927d4dc830a8a5bbbd53b19c5b8c7d5
AL <powerbuoy@gmail.com>
2017-01-30 11:55:56 +0100

    Support for site-logo.png

--- inc/get-site-logo.php
+++ inc/get-site-logo.php
@@ -9,9 +9,13 @@ function sleek_get_site_logo () {
 	if ($customLogoId = get_theme_mod('custom_logo')) {
 		$logo = '<img src="' . wp_get_attachment_image_src($customLogoId, 'full')[0] . '" alt="' . get_bloginfo('name') . '">';
 	}
-	# Check site-logo.svg.php
+	# Check site-logo.svg
 	elseif ($svgLogo = locate_template('dist/assets/svg/site-logo.svg')) {
 		$logo = str_replace('<svg', '<svg aria-label="' . get_bloginfo('name') . '"', file_get_contents($svgLogo)); # TODO: Is aria-label still correct? # TODO: Do I need to remove <?xml etc?
+	}
+	# Check site-logo.png
+	elseif (file_exists(get_stylesheet_directory() . '/dist/assets/site-logo.png')) {
+		$logo = '<img src="' . get_stylesheet_directory_uri() . '/dist/assets/site-logo.png' . '" alt="' . get_bloginfo('name') . '">';
 	}
 	# Default to text (with <tag> support)
 	else {

02ba2172c981a8467036d6ec407f1b9a65498873
AL <powerbuoy@gmail.com>
2017-02-21 13:04:12 +0100

    Thank you Mike :D

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,27 +1,17 @@
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-var minifyCSS = require('gulp-minify-css');
 
 module.exports = function (file, dest) {
 	return gulp.src(file)
-		.pipe(plumber({
-			errorHandler: function (err) {
-				console.log(err);
-				this.emit('end');
-			}
-		}))
 		.pipe(sourcemaps.init())
 		.pipe(sassGlob())
-		.pipe(sass())
+		.pipe(sass({style: 'compressed'}))
+		.on('error', sass.logError)
 		.pipe(autoprefixer({
 			browsers: ['last 1 version', 'IE 9', 'IE 10', '> 2%', 'Safari >= 8']
-		}))
-		.pipe(minifyCSS({
-			advanced: false
 		}))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dest));

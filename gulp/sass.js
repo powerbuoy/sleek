ca62d5512bc97a3674189e4612687d520bd03e9a
AL <powerbuoy@gmail.com>
2017-02-27 16:33:40 +0100

    Compress CSS

--- gulp/sass.js
+++ gulp/sass.js
@@ -8,7 +8,7 @@ module.exports = function (file, dest) {
 	return gulp.src(file)
 		.pipe(sourcemaps.init())
 		.pipe(sassGlob())
-		.pipe(sass({style: 'compressed'}))
+		.pipe(sass({outputStyle: 'compressed'}))
 		.on('error', sass.logError)
 		.pipe(autoprefixer({
 			browsers: ['last 1 version', 'IE 9', 'IE 10', '> 2%', 'Safari >= 8']

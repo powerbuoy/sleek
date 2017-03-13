097e2bfa1491670397d2e68b76ea02a94dfa9fd8
AL <powerbuoy@gmail.com>
2017-03-13 13:20:54 +0100

    Dont print terms if there are none

--- modules/posts.php
+++ modules/posts.php
@@ -15,8 +15,9 @@
 
 					<p>
 						<time datetime="<?php echo get_the_time('Y-m-j') ?>"><?php echo get_the_time(get_option('date_format')) ?></time> |
-						<?php echo get_the_author_meta('display_name') ?> |
-						<?php echo implode(', ', sleek_get_post_terms($post->ID, get_post_type())) ?>
+						<?php echo get_the_author_meta('display_name') ?>
+						<?php $postTerms = sleek_get_post_terms($post->ID, get_post_type()) ?>
+						<?php if ($postTerms) : ?> | <?php echo implode(', ', $postTerms) ?><?php endif ?>
 					</p>
 
 				</header>

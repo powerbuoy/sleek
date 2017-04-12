1ac27029f2c828a32d6688b26a701975448055a5
AL <powerbuoy@gmail.com>
2017-04-13 01:56:02 +0200

    Better styleguide

--- gulp/styleguide/script.js
+++ gulp/styleguide/script.js
@@ -1,12 +1,12 @@
 // Insert all example code into the page so we can actually see the examples
 //window.addEventListener('load', function () {
-	var elements = document.querySelectorAll('section article');
+	var elements = document.querySelectorAll('.styleguide-component');
 
 	for (var i = 0; i < elements.length; i++) {
 		(function () {
 			var element = elements[i];
-			var exampleCode = element.querySelector('.example-code');
-			var example = element.querySelector('.example');
+			var example = element.querySelector('.styleguide-component-example');
+			var exampleCode = element.querySelector('.styleguide-component-code-block');
 
 			if (exampleCode) {
 				example.innerHTML = exampleCode.textContent;

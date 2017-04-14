4b68c36065fde81c29f0bb2eaa6335a86b4b11af
AL <powerbuoy@gmail.com>
2017-04-14 14:25:29 +0200

    better.

--- modules/attachment.php
+++ modules/attachment.php
@@ -1,40 +1,24 @@
+<?php get_template_part('modules/breadcrumbs') ?>
+
 <?php while (have_posts()) : the_post() ?>
 	<section id="attachment">
 
-		<header>
+		<figure>
 
-			<h1><?php the_title() ?></h1>
-
-			<figure>
+			<a href="<?php echo wp_get_attachment_url($post->ID) ?>">
 				<?php echo wp_get_attachment_image($post->ID, 'large') ?>
-			</figure>
+			</a>
 
-			<?php the_excerpt() ?>
+			<figcaption>
 
-		</header>
+				<h1><?php the_title() ?></h1>
 
-		<?php get_template_part('modules/breadcrumbs') ?>
+				<?php the_excerpt() ?>
+				<?php the_content() ?>
 
-		<article>
+			</figcaption>
 
-			<figure>
-
-				<a href="<?php echo wp_get_attachment_url($post->ID) ?>">
-					<?php echo wp_get_attachment_image($post->ID, 'large') ?>
-				</a>
-
-				<figcaption>
-
-					<h2><?php the_title() ?></h2>
-
-					<?php the_excerpt() ?>
-					<?php the_content() ?>
-
-				</figcaption>
-
-			</figure>
-
-		</article>
+		</figure>
 
 	</section>
 <?php endwhile ?>

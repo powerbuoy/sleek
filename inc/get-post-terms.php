13f4cfed1328ff1b135313cf575af8ee7f3ac4f5
AL <powerbuoy@gmail.com>
2017-05-31 15:31:33 +0200

    Also "return" the taxonomy

--- inc/get-post-terms.php
+++ inc/get-post-terms.php
@@ -2,29 +2,34 @@
 /**
  * Returns an array of terms
  */
-function sleek_get_post_terms ($id, $pt, $linked = false, $type = 'category', $field = 'name') {
+function sleek_get_post_terms ($id, $pt, $linked = false, $type = 'category', $field = 'name', &$taxonomy = null) {
 	$terms = [];
 	$tmp = false;
 
 	# Convert tag to correct tax name
 	if ($pt == 'post' and $type == 'tag') {
 		$tmp = wp_get_post_terms($id, 'post_tag');
+		$taxonomy = 'post_tag';
 	}
 	# And category...
 	elseif ($pt == 'post' and $type == 'category') {
 		$tmp = wp_get_post_terms($id, 'category');
+		$taxonomy = 'category';
 	}
 	# Custom taxonomies are assumed to be named post-type-name_category
 	elseif (taxonomy_exists($pt . '_' . $type)) {
 		$tmp = wp_get_post_terms($id, $pt . '_' . $type);
+		$taxonomy = $pt . '_' . $type;
 	}
 	# Also check to see if this pt has the category taxonomy
 	elseif ($type == 'category' and is_object_in_taxonomy($pt, 'category')) {
 		$tmp = wp_get_post_terms($id, 'category');
+		$taxonomy = 'category';
 	}
 	# Also check to see if this pt has the tag taxonomy
 	elseif ($type == 'tag' and is_object_in_taxonomy($pt, 'post_tag')) {
 		$tmp = wp_get_post_terms($id, 'post_tag');
+		$taxonomy = 'post_tag';
 	}
 
 	if ($tmp) {

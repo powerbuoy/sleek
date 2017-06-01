e7c14ea36e09714236d3136f8c914e891d2d717e
AL <powerbuoy@gmail.com>
2017-06-01 15:59:35 +0200

    Dont default to inline svg

--- inc/get-site-logo.php
+++ inc/get-site-logo.php
@@ -4,14 +4,20 @@ add_theme_support('custom-logo', [
 	'header-text' => [get_bloginfo('name'), get_bloginfo('description')]
 ]);
 
-function sleek_get_site_logo () {
+function sleek_get_site_logo ($inlineSvg = false) {
 	# Check custom logo
 	if ($customLogoId = get_theme_mod('custom_logo')) {
 		$logo = '<img src="' . wp_get_attachment_image_src($customLogoId, 'full')[0] . '" alt="' . get_bloginfo('name') . '">';
 	}
 	# Check site-logo.svg
 	elseif ($svgLogo = locate_template('dist/assets/svg/site-logo.svg')) {
-		$logo = str_replace('<svg', '<svg aria-label="' . get_bloginfo('name') . '"', file_get_contents($svgLogo)); # TODO: Is aria-label still correct? # TODO: Do I need to remove <?xml etc?
+		if ($inlineSvg) {
+			# TODO: Is aria-label still correct? # TODO: Should I remove <?xml etc?
+			$logo = str_replace('<svg', '<svg aria-label="' . get_bloginfo('name') . '"', file_get_contents($svgLogo));
+		}
+		else {
+			$logo = '<img src="' . get_stylesheet_directory_uri() . '/dist/assets/svg/site-logo.svg' . '" alt="' . get_bloginfo('name') . '">';
+		}
 	}
 	# Check site-logo.png
 	elseif (file_exists(get_stylesheet_directory() . '/dist/assets/site-logo.png')) {

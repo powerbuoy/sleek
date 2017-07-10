d6eacab42115f5bd452579fe7fac1532c0a93954
AL <powerbuoy@gmail.com>
2017-07-10 23:12:32 +0200

    Woops

--- search.php
+++ search.php
@@ -4,11 +4,9 @@
 
 	<?php get_template_part('modules/archive-header') ?>
 	<?php get_template_part('modules/breadcrumbs') ?>
-<<<<<<< HEAD
 	<?php get_template_part('modules/archive-taxonomies') ?>
-=======
->>>>>>> b4e291948323c2f85145002c8c900146e2ba1a4c
 	<?php get_template_part('modules/posts') ?>
+	<?php get_template_part('modules/posts-pagination') ?>
 
 </main>
 

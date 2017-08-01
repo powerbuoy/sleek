ebad5a256902702d90d5449ef18426030307e006
AL <powerbuoy@gmail.com>
2017-08-01 03:28:33 +0200

    Cleanup

--- modules/header.php
+++ modules/header.php
@@ -1,19 +1,13 @@
 <header id="header">
 
-	<a href="<?php echo home_url('/') ?>" class="site-logo">
+	<a href="<?php echo home_url('/') ?>">
 		<?php echo sleek_get_site_logo() ?>
 	</a>
 
 	<?php if (get_bloginfo('description')) : ?>
-		<p class="tagline">
-			<?php echo str_replace(['&lt;', '&gt;'], ['<', '>'], get_bloginfo('description')) # Support for HTML in site tagline ?>
-		</p>
+		<p><?php echo str_replace(['&lt;', '&gt;'], ['<', '>'], get_bloginfo('description')) # Support for HTML in site tagline ?></p>
 	<?php endif ?>
 
-	<nav>
-		<?php dynamic_sidebar('header') ?>
-	</nav>
-
-	<a href="#header" class="menu-toggle" title="<?php _e('Open menu', 'sleek') ?>" data-toggle-hash></a>
+	<?php dynamic_sidebar('header') ?>
 
 </header>

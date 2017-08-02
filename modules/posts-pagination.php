e5650a203f5fc08bee41a13011ca3e49ea39d7f6
AL <powerbuoy@gmail.com>
2017-08-02 12:54:46 +0200

    Better

---
+++ modules/posts-pagination.php
@@ -0,0 +1,6 @@
+<?php $nav = str_replace('<h2 class="screen-reader-text">REMOVE</h2>', '', get_the_posts_pagination(['screen_reader_text' => 'REMOVE'])) ?>
+<nav id="pagination">
+
+	<?php echo $nav ?>
+
+</nav>
--- modules/posts.php
+++ modules/posts.php
@@ -40,9 +40,4 @@
 
 </section>
 
-<?php $nav = str_replace('<h2 class="screen-reader-text">REMOVE</h2>', '', get_the_posts_pagination(['screen_reader_text' => 'REMOVE'])) ?>
-<nav id="pagination">
-
-	<?php echo $nav ?>
-
-</nav>
+<?php get_template_part('modules/posts-pagination') ?>

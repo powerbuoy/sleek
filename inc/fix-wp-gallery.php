b0b8761d624416dd40d33fa15c9d53e4f3a233ef
AL <powerbuoy@gmail.com>
2017-09-13 14:26:32 +0200

    Better

--- inc/fix-wp-gallery.php
+++ inc/fix-wp-gallery.php
@@ -5,7 +5,7 @@ add_filter('use_default_gallery_style', '__return_false');
 # Wrap images in figure elements
 # https://wordpress.stackexchange.com/questions/174582/always-use-figure-for-post-images
 add_filter('the_content', function ($content) {
-	return preg_replace('/<p>\\s*?(<a rel=\"attachment.*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '<figure>$1</figure>', $content);
+	return preg_replace('/<p>\\s*?(<a.*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '<figure>$1</figure>', $content);
 }, 99);
 
 # Replace the [caption] HTML

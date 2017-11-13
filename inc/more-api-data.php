bb1736abed01c34c7766096c5af70cdf508ea1cd
AL <powerbuoy@gmail.com>
2017-11-13 19:50:34 +0100

    Include custom fields in JSON API for both post _and_ page

--- inc/more-api-data.php
+++ inc/more-api-data.php
@@ -1,7 +1,7 @@
 <?php
 # Add custom fields to rest API
 add_action('rest_api_init', function () {
-	register_rest_field('page', 'custom_fields', ['get_callback' => function () {
+	register_rest_field(['page', 'post'], 'custom_fields', ['get_callback' => function () {
 		return get_post_custom($post['id']);
 	}]);
 });

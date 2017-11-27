6dd8843b1bf2319f20421993fe545ddc6ca2aa2f
AL <powerbuoy@gmail.com>
2017-11-27 12:52:33 +0100

    Use CleanCSS instead of SASS built in outputStyle because the latter fucks up selectors like .bg--white

--- gulp/sass.js
+++ gulp/sass.js
@@ -3,16 +3,18 @@ var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCss = require('gulp-clean-css');
 
 module.exports = function (file, dest) {
 	return gulp.src(file)
 		.pipe(sourcemaps.init())
 		.pipe(sassGlob())
-		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(sass()) // Don't use {outputStyle: 'compressed'} - it fucks up CSS selectors like .bg--white
 		.on('error', sass.logError)
 		.pipe(autoprefixer({
 			browsers: ['last 1 version', 'IE 9', 'IE 10', '> 2%', 'Safari >= 8']
 		}))
+		.pipe(cleanCss())
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dest));
 };

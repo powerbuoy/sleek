0e774d62cc4caf8b94e7fcc28ae7190a1b5227e3
AL <powerbuoy@gmail.com>
2017-11-28 13:54:37 +0100

    Cleanup

--- modules/share-post.php
+++ modules/share-post.php
@@ -1,9 +1,7 @@
-<?php $socialMediaButtons = sleek_get_social_media_links() ?>
-
 <nav id="share-post">
 
 	<ul>
-		<?php foreach ($socialMediaButtons as $smb) : ?>
+		<?php foreach (sleek_get_social_media_links() as $smb) : ?>
 			<li>
 				<a href="<?php echo $smb['url'] ?>" class="icon-<?php echo $smb['slug'] ?>" target="_blank">
 					<?php echo __($smb['title'], 'sleek') ?>

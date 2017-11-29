c7d2035b30e391bebb00a6c5f7fa2166febfc1db
AL <powerbuoy@gmail.com>
2017-11-29 15:15:59 +0100

    Use get_adjacent_post instead + use the_permalink instead of get_permalink

--- modules/next-post.php
+++ modules/next-post.php
@@ -1,12 +1,12 @@
 <?php
-	$next = get_next_post();
-	$next = $next ? $next : get_previous_post();
+	$next = get_adjacent_post();
+	$next = $next ? $next : (get_posts(['post_type' => get_post_type(), 'numberposts' => 1])[0]);
 ?>
 
 <?php if ($next) : ?>
 	<nav id="next-post">
 
-		<a href="<?php echo get_permalink($next->ID) ?>">
+		<a href="<?php the_permalink($next) ?>">
 			<small><?php _e('Next post', 'sleek') ?></small>
 			<?php echo $next->post_title ?>
 		</a>

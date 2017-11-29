a9e018b0499d081c54698f85427525e342cd8167
AL <powerbuoy@gmail.com>
2017-11-29 14:49:55 +0100

    Moved jQuery CDN to reduce requests file

--- inc/reduce-requests.php
+++ inc/reduce-requests.php
@@ -57,3 +57,14 @@ function sleek_reduce_requests ($except = ['wpcf7_js']) {
 		define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
 	}
 }
+
+/**
+ * Move jQuery to bottom of page + include from CDN
+ */
+add_action('wp_enqueue_scripts', function () {
+	if (!is_admin()) {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', '//code.jquery.com/jquery-3.2.1.min.js', false, '3.2.1', true);
+		wp_enqueue_script('jquery');
+	}
+});

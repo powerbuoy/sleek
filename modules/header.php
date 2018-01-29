bee9217d278b01f5abe67feec4a2ba83e359638f
AL <powerbuoy@gmail.com>
2018-01-29 23:50:50 +0100

    Cleanup

--- modules/header.php
+++ modules/header.php
@@ -5,7 +5,7 @@
 	</a>
 
 	<?php if (get_bloginfo('description')) : ?>
-		<p><?php echo str_replace(['&lt;', '&gt;'], ['<', '>'], get_bloginfo('description')) # Support for HTML in site tagline ?></p>
+		<p><?php echo get_bloginfo('description') ?></p>
 	<?php endif ?>
 
 	<?php dynamic_sidebar('header') ?>

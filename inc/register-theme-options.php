2e52d6d996fa53e9d0f45d5ab1a49ba659e13eef
AL <powerbuoy@gmail.com>
2018-01-29 22:12:19 +0100

    Removed gtag, google analytics, tag manager support and added <head>, <body> and </body> textareas for any type of script instead

--- inc/register-theme-options.php
+++ inc/register-theme-options.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Register some options built into sleek (see register-assets for how these settings are used)
+ * Register some options built into sleek
  */
  add_action('customize_register', function ($wpCustomize) {
  	$wpCustomize->add_section('sleek_settings', [
@@ -9,9 +9,10 @@
  	]);
 
 	sleek_register_theme_options($wpCustomize, [
-		'google_gtag_id' => 'text',
-		'google_analytics_id' => 'text',
-		'google_tag_manager_id' => 'text',
+		'head_code' => 'textarea',
+		'body_code' => 'textarea',
+		'foot_code' => 'textarea',
+
 		'google_maps_api_key' => 'text',
 		'google_search_api_key' => 'text',
 		'google_search_engine_id' => 'text'
@@ -52,74 +53,33 @@ function sleek_register_theme_options ($wpCustomize, $options, $textdomain = 'sl
 }
 
 /**
- * Add Google Maps if specified in theme options
- */
-add_action('wp_enqueue_scripts', function () {
-	if ($googleMaps = get_theme_mod('google_maps_api_key')) {
-		wp_register_script('google_maps', 'https://maps.googleapis.com/maps/api/js?key=' . $googleMaps . '&callback=gmAsyncInit', [], null, true);
-		wp_enqueue_script('google_maps');
-	}
-});
-
-/**
  * Add some stuff to <head></head>
  */
 add_action('wp_head', function () {
-	# Gtag
-	if ($gtag = get_theme_mod('google_gtag_id')) {
-		echo "<script async src=\"https://www.googletagmanager.com/gtag/js?id=$gtag\"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-
-			gtag('config', '$gtag');
-		</script>";
-	}
-
-	# Google Analytics
-	if ($googleAnalytics = get_theme_mod('google_analytics_id')) {
-		echo "<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			ga('create', '$googleAnalytics', 'auto');
-			ga('send', 'pageview');
-		</script>";
-	}
-
-	# Google Tag Manager
-	if ($gtmId = get_theme_mod('google_tag_manager_id')) {
-		echo "<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','$gtmId');
-		</script>";
+	if ($code = get_theme_mod('head_code')) {
+		echo $code;
 	}
 });
 
 /**
- * Add some stuff before <body>
+ * Add some just after <body>
  */
 add_action('sleek_after_body_tag_open', function () {
-	# Google Tag Manager again
-	if ($gtmId = get_theme_mod('google_tag_manager_id')) {
-		echo '<noscript>
-			<iframe src="https://www.googletagmanager.com/ns.html?id='. $gtmId .'" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-		</noscript>';
+	if ($code = get_theme_mod('body_code')) {
+		echo $code;
 	}
 });
 
 /**
- * Add some stuff before </body>
+ * Add some stuff right before </body>
  */
 add_action('wp_footer', function () {
+	if ($code = get_theme_mod('foot_code')) {
+		echo $code;
+	}
+
 	# Google Maps
-	if ($googleMaps = get_theme_mod('google_maps_api_key')) {
+	if (get_theme_mod('google_maps_api_key')) {
 		echo "<script>
 			window.gmAsyncInit = function () {};
 
@@ -137,6 +97,16 @@ add_action('wp_footer', function () {
 				}
 			}
 		</script>";
+	}
+});
+
+/**
+ * Add Google Maps if specified in theme options
+ */
+add_action('wp_enqueue_scripts', function () {
+	if ($googleMaps = get_theme_mod('google_maps_api_key')) {
+		wp_register_script('google_maps', 'https://maps.googleapis.com/maps/api/js?key=' . $googleMaps . '&callback=gmAsyncInit', [], null, true);
+		wp_enqueue_script('google_maps');
 	}
 });
 

b9ae5cea0219235279d1e4aa2c68e40bab3d1fad
AL <powerbuoy@gmail.com>
2018-02-20 01:37:40 +0100

    Only show post meta on the POST PT

--- modules/post.php
+++ modules/post.php
@@ -11,17 +11,19 @@
 				</figure>
 			<?php endif ?>
 
-			<p>
-				<time datetime="<?php echo get_the_time('Y-m-j') ?>">
-					<?php echo get_the_time(get_option('date_format')) ?>
-				</time>
-				<?php if ($terms = sleek_get_post_terms($post->ID, get_post_type(), true)) : ?>
-					<span><?php echo implode(', ', $terms) ?></span>
-				<?php endif ?>
-				<?php if (get_the_author_meta('ID') != 1) : ?>
-					<?php the_author_posts_link() ?>
-				<?php endif ?>
-			</p>
+			<?php if (get_post_type() == 'post') : ?>
+				<p>
+					<time datetime="<?php echo get_the_time('Y-m-j') ?>">
+						<?php echo get_the_time(get_option('date_format')) ?>
+					</time>
+					<?php if ($terms = sleek_get_post_terms($post->ID, get_post_type(), true)) : ?>
+						<span><?php echo implode(', ', $terms) ?></span>
+					<?php endif ?>
+					<?php if (get_the_author_meta('ID') != 1) : ?>
+						<?php the_author_posts_link() ?>
+					<?php endif ?>
+				</p>
+			<?php endif ?>
 
 		</header>
 

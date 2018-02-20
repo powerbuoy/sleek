94b70739608082694622f83c7dd1f7f4eb35d423
AL <powerbuoy@gmail.com>
2018-02-20 01:37:53 +0100

    NavXT and Yoast Breadcrumbs support

--- modules/breadcrumbs.php
+++ modules/breadcrumbs.php
@@ -1,1 +1,9 @@
-<?php if (function_exists('yoast_breadcrumb')) yoast_breadcrumb('<nav id="breadcrumbs">', '</nav>') ?>
+<?php if (function_exists('bcn_display')) : ?>
+	<nav id="breadcrumbs">
+		<?php bcn_display() ?>
+	</nav>
+<?php elseif (function_exists('yoast_breadcrumb')) : ?>
+	<nav id="breadcrumbs">
+		<?php yoast_breadcrumb('', '') ?>
+	</nav>
+<?php endif ?>

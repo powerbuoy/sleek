44669942bcbbc84e33725964dee90aaecb1e5432
AL <powerbuoy@gmail.com>
2018-10-25 15:52:33 +0200

    Doesnt  wrk

--- inc/disable-theme-updates.php
+++ inc/disable-theme-updates.php
@@ -3,7 +3,7 @@
 # https://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
 add_filter('http_request_args', function ($request, $url) {
 	# Is a theme update-check happening?
-	if (strpos($url, 'http://api.wordpress.org/themes/update-check') === 0) {
+	if (strpos($url, 'http://api.wordpress.org/themes/update-check') === 0 or strpos($url, 'https://api.wordpress.org/themes/update-check') === 0) {
 		# Grab list of themes
 		$themes = unserialize($request['body']['themes']);
 

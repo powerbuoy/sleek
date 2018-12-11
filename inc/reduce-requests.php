0926e1a0b2e04dfeb6c92c045f81820e40935364
AL <powerbuoy@gmail.com>
2018-12-11 20:49:00 +0100

    Reduce requests

--- inc/reduce-requests.php
+++ inc/reduce-requests.php
@@ -57,6 +57,8 @@ add_action('init', function () {
 
 		# WPML Language Switcher
 		define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
+		define('ICL_DONT_LOAD_NAVIGATION_CSS', true);
+		define('ICL_DONT_LOAD_LANGUAGES_JS', true);
 	}
 });
 

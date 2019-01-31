3151512b3cae7dbdc61bf8e3258d179bdee53cfd
AL <powerbuoy@gmail.com>
2019-01-31 23:03:17 +0100

    Use new util instead of hard coding for social menu

--- modules/social-menu.php
+++ modules/social-menu.php
@@ -1,30 +1,4 @@
-<?php
-	$tmp = get_option('wpseo_social');
-	$links = [];
-	$nicenames = [
-		'linkedin' => 'LinkedIn',
-		'youtube' => 'YouTube',
-		'google_plus' => 'Google+'
-	];
-
-	if ($tmp and count($tmp)) {
-		foreach ($tmp as $k => $v) {
-			# Only grab non empty URLs or "sites"
-			if ((substr($k, -3) === 'url' or substr($k, -4) === 'site') and !empty($v)) {
-				$url = $k === 'twitter_site' ? 'https://twitter.com/' . $v : $v;
-				$name = substr($k, -3) === 'url' ? $name = substr($k, 0, -4) : $name = substr($k, 0, -5);
-				$name = isset($nicenames[$name]) ? $nicenames[$name] : ucfirst(str_replace(['_', '-'], ' ', $name));
-
-				$links[] = [
-					'name' => $name,
-					'url' => $url
-				];
-			}
-		}
-	}
-?>
-
-<?php if (count($links)) : ?>
+<?php if (($links = sleek_get_yoast_social_links()) and count($links)) : ?>
 	<nav id="social-menu">
 
 		<ul>

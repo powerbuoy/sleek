d12093048e32c822a14b8dfbefdb25ac96223f92
AL <powerbuoy@gmail.com>
2019-05-25 16:02:27 +0200

    Better default zoom level for map and dont render map without lat/lng

--- acf/google-map/default.php
+++ acf/google-map/default.php
@@ -1,6 +1,10 @@
 <?php $options = get_option(SLEEK_SETTINGS_NAME) ?>
 
-<?php if (isset($options['google_maps_api_key']) and !empty($options['google_maps_api_key'])) : ?>
+<?php if (
+	isset($options['google_maps_api_key']) and !empty($options['google_maps_api_key']) and
+	isset($google_map['lat']) and !empty($google_map['lat']) and
+	isset($google_map['lng']) and !empty($google_map['lng'])
+) : ?>
 	<section id="google-map">
 
 		<?php if ($google_map_title or $google_map_description) : ?>
@@ -19,7 +23,7 @@
 			<img src="https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&amp;center=<?php echo $google_map['lat'] ?>,<?php echo $google_map['lng'] ?>&amp;markers=<?php echo $google_map['lat'] ?>,<?php echo $google_map['lng'] ?>&amp;zoom=14&amp;size=600x400&amp;key=<?php echo $options['google_maps_api_key'] ?>">
 		</noscript>
 
-		<div class="google-map" data-lat="<?php echo $google_map['lat'] ?>" data-lng="<?php echo $google_map['lng'] ?>">
+		<div class="google-map" data-lat="<?php echo $google_map['lat'] ?>" data-lng="<?php echo $google_map['lng'] ?>" data-zoom="12">
 
 			<?php if ($google_map_info_window) : ?>
 				<div data-lat="<?php echo $google_map['lat'] ?>" data-lng="<?php echo $google_map['lng'] ?>">

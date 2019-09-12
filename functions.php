cda60bd78e49281ffeac16b93c37992192318d41
AL <powerbuoy@gmail.com>
2019-09-12 11:59:08 +0200

    Moar

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+###################
+# Composer Autoload
+require __DIR__ . '/vendor/autoload.php';
+
+########################
+# Set up for translation
+add_action('after_setup_theme', function () {
+	load_theme_textdomain('sleek', get_template_directory() . '/languages');
+});
+
+################
+# My custom code
+# TODO

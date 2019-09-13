b67f697976d8510fdf178c3dd4ee19333d3222e9
AL <powerbuoy@gmail.com>
2019-09-13 14:49:33 +0200

    Moar

--- functions.php
+++ functions.php
@@ -5,18 +5,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 ######################################
 # Modify WP's built in thumbnail sizes
-add_action('after_setup_theme', function () {
-	Sleek\ImageSizes\register(1920, 1080, ['center', 'center']/*, [
-		'portrait' => ['width' => 1080, 'height' => 1920, 'crop' => ['center', 'top']],
-		'square' => ['width' => 1920, 'height' => 1920],
-	]*/);
-});
+Sleek\ImageSizes\register(1920, 1080, ['center', 'center']/*, [
+	'portrait' => ['width' => 1080, 'height' => 1920, 'crop' => ['center', 'top']],
+	'square' => ['width' => 1920, 'height' => 1920],
+]*/);
 
 ########################
 # Set up for translation
-add_action('after_setup_theme', function () {
-	load_theme_textdomain('sleek', get_template_directory() . '/languages');
-});
+load_theme_textdomain('sleek', get_template_directory() . '/languages');
 
 ################
 # 404 some pages

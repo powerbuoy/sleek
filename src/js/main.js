09ec6f1218010ed734c4cb9bca8ff4a236cdf3aa
AL <powerbuoy@gmail.com>
2019-09-19 04:22:41 +0200

    webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+import '../sass/main.scss';
+
 import defFunc from './module';
+import SleekMap from 'sleek-map';
 
 defFunc();
+
+console.dir(SleekMap);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
+/*
+	TODO:
+	- sourcemaps in dev
+	- style-loader in dev
+	- watch
+	- files??
+	- glob-import in sass and js (main should auto-import every other file in js/ and main.scss can do @import "components/*")
+	- icons (svg? fontello?)
+	- gettext
+*/
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+	// In
 	entry: './src/js/main.js',
+
+	// n Out
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist')
 	},
+
+	// Plug-ins
+	plugins: [
+		// CSS Extractor
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+
+	// Config
 	module: {
 		rules: [
+			// JS
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -17,6 +42,35 @@ module.exports = {
 						presets: ['@babel/preset-env']
 					}
 				}
+			},
+
+			// SASS
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					// Extract CSS from main.js and create main.css
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					// Enable importing CSS
+					{
+						loader: 'css-loader'
+					},
+					// PostCSS (autoprefixer etc)
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [
+								require('autoprefixer')
+							]
+						}
+					},
+					// SASS
+					{
+						loader: 'sass-loader'
+					}
+				]
 			}
 		]
 	}

82bea3e916a2bab5b56036eb0a0309288f1c90e7
AL <powerbuoy@gmail.com>
2019-09-30 14:45:36 +0200

    Moar

--- modules/archive.php
+++ modules/archive.php
@@ -2,11 +2,11 @@
 
 	<header>
 
-		<?php /* TODO: if ($image = Sleek\ArchiveMeta\get_the_archive_image('large')) : ?>
+		<?php if ($image = Sleek\ArchiveMeta\get_the_archive_image('large')) : ?>
 			<figure>
 				<?php echo $image ?>
 			</figure>
-		<?php endif */ ?>
+		<?php endif ?>
 
 		<h1><?php the_archive_title() ?></h1>
 

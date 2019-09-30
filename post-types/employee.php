c822525014acca72b1346404ed287dbdc66a798d
AL <powerbuoy@gmail.com>
2019-09-30 13:39:08 +0200

    Moar

--- post-types/employee.php
+++ post-types/employee.php
@@ -5,8 +5,6 @@ class Employee extends PostType {
 	public function config () {
 		return [
 			'menu_icon' => 'dashicons-groups',
-			'public' => false,
-			'show_ui' => true,
 			'taxonomies' => ['employee_tag', 'city']
 		];
 	}

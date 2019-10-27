92d86a2ff710554ad317ee600b01f4a230fbcd93
AL <powerbuoy@gmail.com>
2019-10-27 20:45:28 +0100

    Cleanup

--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -1,18 +1,16 @@
-(function () {
-	'use strict';
+'use strict';
 
-	var observer = new IntersectionObserver(function (entries) {
-		entries.forEach(function (entry) {
-			if (entry.isIntersecting) {
-				entry.target.classList.add('in-view', 'was-in-view');
-			}
-			else {
-				entry.target.classList.remove('in-view');
-			}
-		});
-	}, {threshold: .25});
-
-	document.querySelectorAll('section, article, header, footer, nav, aside, div, h1, h2, h3, h4, h5, h6, figure').forEach(function (el) {
-		observer.observe(el);
+var observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('in-view', 'was-in-view');
+		}
+		else {
+			entry.target.classList.remove('in-view');
+		}
 	});
-})();
+}, {threshold: .25});
+
+document.querySelectorAll('section, article, header, footer, nav, aside, div, h1, h2, h3, h4, h5, h6, figure').forEach(el => {
+	observer.observe(el);
+});

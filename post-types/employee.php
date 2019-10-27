64484a5e2403ba2034f877080ffb79234b213376
AL <powerbuoy@gmail.com>
2019-10-27 14:14:08 +0100

    Use proper filters instead of home made solutions

--- post-types/employee.php
+++ post-types/employee.php
@@ -2,16 +2,21 @@
 namespace Sleek\PostTypes;
 
 class Employee extends PostType {
+	public function created () {
+		add_filter('register_taxonomy_args', function ($args, $taxonomy, $object_type) {
+			if ($taxonomy === 'employee_tag') {
+				$args['public'] = false;
+				$args['show_ui'] = true;
+			}
+
+			return $args;
+		}, 10, 3);
+	}
+
 	public function config () {
 		return [
 			'menu_icon' => 'dashicons-groups',
-			'taxonomies' => ['employee_tag', 'city'],
-			'taxonomy_config' => [
-				'employee_tag' => [
-					'public' => false,
-					'show_ui' => true
-				]
-			]
+			'taxonomies' => ['employee_tag', 'city']
 		];
 	}
 }

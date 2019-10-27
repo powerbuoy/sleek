ec5c9dd31fe76ad76cc7f131218dab81aff053e9
AL <powerbuoy@gmail.com>
2019-10-27 22:53:32 +0100

    Cleanup

--- front-page.php
+++ front-page.php
@@ -3,16 +3,6 @@
 <main>
 
 	<?php get_template_part('modules/single-page') ?>
-
-	<div data-vue>
-		<vue-component></vue-component>
-	</div>
-
-	<?php /* Sleek\Modules\render('share-page', get_the_ID()) ?>
-	<?php (new Sleek\Modules\SharePage(['title' => 'Share!!', 'services' => ['Facebook', 'Twitter']]))->render() ?>
-	<?php Sleek\Modules\render('social-links', ['title' => 'Socialize!']) ?>
-	<?php (new Sleek\Modules\SocialLinks(['title' => 'Socialize!!']))->render() ?>
-	<?php Sleek\Modules\render('text-block', ['title' => 'TextBlock']) */ ?>
 	<?php Sleek\Modules\render_flexible('modules_below_content', get_the_ID()) ?>
 
 </main>

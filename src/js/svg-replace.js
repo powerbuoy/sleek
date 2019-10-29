bdb933a377568b0e1c65341dff469cf271f050bd
AL <powerbuoy@gmail.com>
2019-10-29 04:24:37 +0100

    MOar

--- src/js/svg-replace.js
+++ src/js/svg-replace.js
@@ -3,7 +3,10 @@
 document.querySelectorAll('[data-svg]').forEach(el => {
 	const svg = el.dataset.svg;
 
+	el.style.visibility = 'hidden';
+
 	fetch(SLEEK_STYLESHEET_DIRECTORY_URI + '/dist/assets/' + svg + '.svg').then(res => res.text()).then(text => {
+		el.style.visibility = 'visible';
 		el.innerHTML = text;
 
 		el.querySelector('svg').classList.add('svg-stroke-animation');

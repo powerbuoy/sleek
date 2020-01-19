31f1c359c975a70b27214d7144053cf2749454aa
powerbuoy <powerbuoy@gmail.com>
2020-01-19 16:39:11 +0100

    Fix

--- front-page.php
+++ front-page.php
@@ -3,8 +3,8 @@
 <main>
 
 	<?php get_template_part('modules/single-page') ?>
-	<?php # Sleek\Modules\render_flexible('modules_below_content', get_the_ID()) ?>
-	<?php Sleek\Modules\render_dummies(['attachments', 'contact-form', 'featured-posts', 'gallery', 'google-map', 'hubspot-form', 'instagram', 'latest-posts', 'next-post', 'page-menu', 'related-pages', 'related-posts', 'share-page', 'social-links', 'text-block', 'text-blocks', 'users', 'video']) ?>
+	<?php Sleek\Modules\render_flexible('modules_below_content', get_the_ID()) ?>
+	<?php # Sleek\Modules\render_dummies(['attachments', 'contact-form', 'featured-posts', 'gallery', 'google-map', 'hubspot-form', 'instagram', 'latest-posts', 'next-post', 'page-menu', 'related-pages', 'related-posts', 'share-page', 'social-links', 'text-block', 'text-blocks', 'users', 'video']) ?>
 
 </main>
 

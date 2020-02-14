e5bac8531fef045149c3467899bf473b2bbc4461
powerbuoy <powerbuoy@gmail.com>
2020-02-14 13:56:23 +0100

    New dialog

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,35 +1,61 @@
-import dialogPolyfill from 'dialog-polyfill';
+// Init dialogs
+document.querySelectorAll('.dialog').forEach(el => {
+	// Insert methods
+	el.sleekDialog = {
+		open: () => {
+			document.documentElement.classList.add('dialog-open');
+			document.documentElement.classList.add('dialog-open--' + el.id);
+			el.classList.add('open');
+		},
+		close: () => {
+			document.documentElement.classList.remove('dialog-open');
+			document.documentElement.classList.remove('dialog-open--' + el.id);
+			el.classList.remove('open');
+		},
+		isOpen: () => {
+			return el.classList.contains('open');
+		},
+		isClosed: () => {
+			return !el.classList.contains('open');
+		},
+		getStatus: () => {
+			return el.classList.contains('open') ? 'open' : 'close';
+		}
+	};
 
-// For every dialog
-document.querySelectorAll('dialog').forEach(el => {
-	// Polyfill it
-	dialogPolyfill.registerDialog(el);
+	// Insert backdrop
+	var backdrop = document.createElement('div');
 
-	// And add a close button
-	const close = document.createElement('a');
+	backdrop.classList.add('backdrop');
+	el.parentNode.insertBefore(backdrop, el.nextSibling);
+
+	backdrop.addEventListener('click', e => {
+		if (e.target === backdrop) {
+			el.sleekDialog.close();
+		}
+	});
+
+	// Insert close button
+	var close = document.createElement('a');
 
 	close.classList.add('close');
 	close.innerHTML = '&times;';
 	close.addEventListener('click', e => {
 		e.preventDefault();
-		el.close();
+		el.sleekDialog.close();
 	});
 	el.appendChild(close);
 });
 
-// For every [data-dialog] link
-document.querySelectorAll('[data-dialog], a[href$="-popup"]').forEach(el => {
-	const targetId = el.dataset.popup || el.getAttribute('href');
+// Init triggers
+document.querySelectorAll('a[href$="-dialog"]').forEach(el => {
+	el.addEventListener('click', e => {
+		e.preventDefault();
 
-	if (targetId) {
-		const target = document.getElementById(targetId.substr(1));
+		var target = document.getElementById(el.getAttribute('href').substr(1));
 
-		// Open the target popup on click
 		if (target) {
-			el.addEventListener('click', e => {
-				e.preventDefault();
-				target.showModal();
-			});
+			target.sleekDialog.open();
 		}
-	}
+	});
 });

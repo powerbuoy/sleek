2b11e1020a092c0bdd2b392262b52f7c1490f12f
powerbuoy <powerbuoy@gmail.com>
2020-02-27 00:20:40 +0100

    Dialog better

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,25 +1,31 @@
+// Create template dialog
+const templateDialog = document.createElement('div');
+
+templateDialog.classList.add('dialog');
+document.body.appendChild(templateDialog);
+
 // Init dialogs
-document.querySelectorAll('.dialog').forEach(el => {
+document.querySelectorAll('.dialog').forEach(dialog => {
 	// Insert methods
-	el.sleekDialog = {
+	dialog.sleekDialog = {
 		open: () => {
 			document.documentElement.classList.add('dialog-open');
-			document.documentElement.classList.add('dialog-open--' + el.id);
-			el.classList.add('open');
+			document.documentElement.classList.add('dialog-open--' + dialog.id);
+			dialog.classList.add('open');
 		},
 		close: () => {
 			document.documentElement.classList.remove('dialog-open');
-			document.documentElement.classList.remove('dialog-open--' + el.id);
-			el.classList.remove('open');
+			document.documentElement.classList.remove('dialog-open--' + dialog.id);
+			dialog.classList.remove('open');
 		},
 		isOpen: () => {
-			return el.classList.contains('open');
+			return dialog.classList.contains('open');
 		},
 		isClosed: () => {
-			return !el.classList.contains('open');
+			return !dialog.classList.contains('open');
 		},
 		getStatus: () => {
-			return el.classList.contains('open') ? 'open' : 'close';
+			return dialog.classList.contains('open') ? 'open' : 'close';
 		}
 	};
 
@@ -27,24 +33,27 @@ document.querySelectorAll('.dialog').forEach(el => {
 	var backdrop = document.createElement('div');
 
 	backdrop.classList.add('backdrop');
-	el.parentNode.insertBefore(backdrop, el.nextSibling);
+	dialog.parentNode.insertBefore(backdrop, dialog.nextSibling);
 
 	backdrop.addEventListener('click', e => {
 		if (e.target === backdrop) {
-			el.sleekDialog.close();
+			dialog.sleekDialog.close();
 		}
 	});
 
 	// Insert close button
 	var close = document.createElement('a');
 
-	close.classList.add('close');
+	close.classList.add('dialog__close');
 	close.innerHTML = '&times;';
-	close.addEventListener('click', e => {
-		e.preventDefault();
-		el.sleekDialog.close();
+	dialog.appendChild(close);
+
+	dialog.addEventListener('click', e => {
+		if (e.target.classList.contains('dialog__close')) {
+			e.preventDefault();
+			dialog.sleekDialog.close();
+		}
 	});
-	el.appendChild(close);
 });
 
 // Init triggers
@@ -55,7 +64,21 @@ document.querySelectorAll('a[href$="-dialog"]').forEach(el => {
 		var target = document.getElementById(el.getAttribute('href').substr(1));
 
 		if (target) {
-			target.sleekDialog.open();
+			// The target is a template
+			if (target.nodeName.toLowerCase() === 'script') {
+				templateDialog.className = 'dialog dialog--no-transition ' + target.className;
+				templateDialog.innerHTML = target.innerHTML + '<a class="dialog__close">&times;</a>';
+
+				// HACK: Wait for dialog--no-transition to kick in (for some reason I need around 50ms...)
+				setTimeout(() => {
+					templateDialog.classList.remove('dialog--no-transition');
+					templateDialog.sleekDialog.open();
+				}, 50);
+			}
+			// The target is a dialog element
+			else {
+				target.sleekDialog.open();
+			}
 		}
 	});
 });

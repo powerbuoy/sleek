3030243f4f8f7b122c0eb3cec9c89bc26dcce84c
powerbuoy <powerbuoy@gmail.com>
2020-03-06 00:34:53 +0100

    Better examples

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
 <main>
 
 	<?php get_template_part('modules/single-page') ?>
+	<?php # Sleek\Modules\render_dummies(array_map('basename', array_filter(glob(get_stylesheet_directory() . '/modules/*'), 'is_dir'))) ?>
 	<?php # Sleek\Modules\render_flexible('flexible_modules') ?>
 
 </main>

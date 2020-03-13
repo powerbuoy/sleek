95e01e8dd5b793fba9f66d89925f3f3a89dd1b6d
powerbuoy <powerbuoy@gmail.com>
2020-03-13 20:25:59 +0100

    Build Module Thumbs

---
+++ build-module-thumbs.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../../../wp-load.php';
+
+$domain = home_url();
+$path = get_stylesheet_directory() . '/modules/*';
+$command = '"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --screenshot="%s" --window-size=1280,1280 --default-background-color=0 %s';
+
+echo "Generating thumbnails for $domain...";
+
+foreach (glob($path) as $modulePath) {
+	if (is_dir($modulePath)) {
+		$moduleName = pathinfo($modulePath, PATHINFO_FILENAME);
+
+		foreach (glob($modulePath . '/*.php') as $templatePath) {
+			$templateName = pathinfo($templatePath, PATHINFO_FILENAME);
+
+			if ($templateName !== 'module') {
+				echo "\n\nGenerating thumbnail for $moduleName/$templateName...\n";
+
+				shell_exec(sprintf($command, $modulePath . "/$templateName.png", $domain . "/__SLEEK__/modules/dummy-module-preview/$moduleName/$templateName/"));
+			}
+		}
+	}
+}

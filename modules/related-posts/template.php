783339440ded35938d332907bc50ce7bb9200fe0
powerbuoy <powerbuoy@gmail.com>
2020-03-19 17:24:01 +0100

    Cleanup module t emplates

--- modules/related-posts/template.php
+++ modules/related-posts/template.php
@@ -1,21 +1,23 @@
-<?php if ($rows) : ?>
-	<section id="related-posts">
+<section id="related-posts">
 
-		<?php if ($title or $description) : ?>
-			<header>
+	<?php if ($title or $description) : ?>
+		<header>
 
-				<?php if ($title) : ?>
-					<h2><?php echo $title ?></h2>
-				<?php endif ?>
+			<?php if ($title) : ?>
+				<h2><?php echo $title ?></h2>
+			<?php endif ?>
 
-				<?php echo $description ?>
+			<?php echo $description ?>
 
-			</header>
-		<?php endif ?>
+		</header>
+	<?php endif ?>
 
+	<?php if ($rows) : ?>
 		<?php foreach ($rows as $post) : setup_postdata($post) ?>
 			<?php get_template_part('modules/post', get_post_type()) ?>
 		<?php endforeach; wp_reset_postdata() ?>
+	<?php else : ?>
+		<p class="error"><?php _e('No related posts found.', 'sleek') ?></p>
+	<?php endif ?>
 
-	</section>
-<?php endif ?>
+</section>
--- modules/text-block/template.php
+++ modules/text-block/template.php
@@ -1,18 +1,14 @@
 <section id="text-block">
 
-	<header>
+	<?php if ($image) : ?>
+		<figure>
+			<?php echo wp_get_attachment_image($image, 'large') ?>
+		</figure>
+	<?php endif ?>
 
-		<?php if ($image) : ?>
-			<figure>
-				<?php echo wp_get_attachment_image($image, 'large') ?>
-			</figure>
-		<?php endif ?>
-
-		<?php if ($title) : ?>
-			<h2><?php echo $title ?></h2>
-		<?php endif ?>
-
-	</header>
+	<?php if ($title) : ?>
+		<h2><?php echo $title ?></h2>
+	<?php endif ?>
 
 	<?php echo $description ?>
 

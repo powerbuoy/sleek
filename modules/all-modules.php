0562f62b26e6e4266cf91dc64b57989c93933139
powerbuoy <powerbuoy@gmail.com>
2020-03-19 22:05:20 +0100

    Moar style

--- modules/all-modules.php
+++ modules/all-modules.php
@@ -1,1 +1,2 @@
-<?php Sleek\Modules\render_dummies(array_map('basename', array_filter(glob(get_stylesheet_directory() . '/modules/*'), 'is_dir'))) ?>
+<?php # Sleek\Modules\render_dummies(array_map('basename', array_filter(glob(get_stylesheet_directory() . '/modules/*'), 'is_dir'))) ?>
+<?php Sleek\Modules\render_dummies(['text-blocks']) ?>

926f9308cc8e036c3be4876457c3af9345723cc5
powerbuoy <powerbuoy@gmail.com>
2020-04-02 15:02:54 +0200

    Enable dialog

--- src/js/app.js
+++ src/js/app.js
@@ -95,7 +95,7 @@ document.querySelectorAll('[data-toggle-hash]').forEach(el => {
 
 /////////
 // Dialog
-/* import Dialog, { DialogTrigger } from 'sleek-ui/src/js/dialog';
+import Dialog, { DialogTrigger } from 'sleek-ui/src/js/dialog';
 
 const templateDialog = document.createElement('div');
 templateDialog.classList.add('dialog');
@@ -110,7 +110,7 @@ document.querySelectorAll('a[href^="#dialog-"]').forEach(el => {
 
 document.querySelectorAll('div.dialog').forEach(el => {
 	new Dialog(el).mount();
-}); */
+});
 
 ////////////
 // Slideshow

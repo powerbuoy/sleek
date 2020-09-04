4758c3563208e8da6ad3c1568347eecb6dd696a6
powerbuoy <powerbuoy@gmail.com>
2020-09-04 12:16:02 +0200

    New instagram plugin / module

--- modules/instagram/module.php
+++ modules/instagram/module.php
@@ -2,12 +2,6 @@
 namespace Sleek\Modules;
 
 class Instagram extends Module {
-	public function init () {
-		$this->dummy_data();
-
-	#	add_filter('wpiw_proxy', '__return_true');
-	}
-
 	public function fields () {
 		return [
 			[
@@ -21,30 +15,11 @@ class Instagram extends Module {
 				'type' => 'wysiwyg'
 			],
 			[
-				'name' => 'username',
-				'label' => __('Instagram Username', 'sleek'),
-				'instructions' => __('Enter the Instagram username here. Please note that this module requires the WP Instagram Widget plug-in: https://github.com/scottsweb/wp-instagram-widget', 'sleek'),
-				'type' => 'text'
-			],
-			[
 				'name' => 'limit',
 				'label' => __('Number of Images', 'sleek'),
 				'type' => 'number',
 				'default_value' => 4
 			]
 		];
-	}
-
-	private function dummy_data () {
-		add_filter('sleek/modules/dummy_field_value', function ($value, $field, $module) {
-			if ($field['name'] === 'username' and $module === 'instagram') {
-				return 'albertdrosphotography';
-			}
-			elseif ($field['name'] === 'limit' and $module === 'instagram') {
-				return 4;
-			}
-
-			return $value;
-		}, 10, 3);
 	}
 }
--- modules/instagram/template.php
+++ modules/instagram/template.php
@@ -1,4 +1,4 @@
-<?php if (class_exists('null_instagram_widget')) : ?>
+<?php if (class_exists('Meks_Instagram_Widget')) : ?>
 	<section id="instagram">
 
 		<?php if ($title or $description) : ?>
@@ -13,9 +13,10 @@
 			</header>
 		<?php endif ?>
 
-		<?php the_widget('null_instagram_widget', 'username=' . $username . '&number=' . $limit . '&target=_blank') ?>
+		<?php $html = do_shortcode("[meks_easy_photo_feed photos_number=$limit]") ?>
+		<?php echo preg_replace('/(<[^>]*) style=("[^"]+"|\'[^\']+\')([^>]*>)/i', '$1$3', $html) ?>
 
 	</section>
 <?php else : ?>
-	<p class="error"><?php printf(__('Please make sure to activate the WP Instagram Widget to enable this module: %s', 'sleek'), '<a href="https://github.com/scottsweb/wp-instagram-widget">github.com/scottsweb/wp-instagram-widget</a>') ?></p>
+	<p class="error"><?php printf(__('Please make sure to activate the Meks Easy Photo Feed Widget to enable this module: %s', 'sleek'), '<a href="https://wordpress.org/plugins/meks-easy-instagram-widget/">wordpress.org/plugins/meks-easy-instagram-widget/</a>') ?></p>
 <?php endif ?>

0b76a56c71ff5895a38de9fbe886642c7b80ee3c
Andreas Lagerkvist <powerbuoy@gmail.com>
2020-11-13 11:54:34 +0100

    Dont look for .vue files in src

--- modules/vue-component/module.php
+++ modules/vue-component/module.php
@@ -21,23 +21,8 @@ class VueComponent extends Module {
 			[
 				'name' => 'component',
 				'label' => __('Component', 'sleek'),
-				'type' => 'select',
-				'choices' => $this->getVueComponents(),
-				'ui' => true,
-				'allow_null' => true,
-				'multiple' => false
+				'type' => 'text'
 			]
 		];
-	}
-
-	protected function getVueComponents () {
-		$components = [];
-
-		foreach (glob(get_stylesheet_directory() . '/src/js/*.vue') as $file) {
-			$pathinfo = pathinfo($file);
-			$components[$pathinfo['filename']] = \Sleek\Utils\convert_case($pathinfo['filename'], 'title');
-		}
-
-		return $components;
 	}
 }

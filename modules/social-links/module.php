9b5d195d270ce1f4eaee90334c1311c5b1b2aa5e
powerbuoy <powerbuoy@gmail.com>
2024-02-22 12:41:58 +0100

    Update social links so they work with latest yoast

--- modules/social-links/module.php
+++ modules/social-links/module.php
@@ -29,11 +29,71 @@ class SocialLinks extends Module {
 
 	public static function get_links () {
 		$tmp = get_option('wpseo_social');
+		\Sleek\Utils\log($tmp);
 		$links = [];
 		$nicenames = [
 			'linkedin' => 'LinkedIn',
 			'youtube' => 'YouTube',
 			'google_plus' => 'Google+'
+		];
+
+		$urlMap = [
+			'500px.com' => '500px',
+			'amazon.' => 'amazon',
+			'apple.com' => 'apple',
+			'itunes.com' => 'apple',
+			'bandcamp.com' => 'bandcamp',
+			'behance.net' => 'behance',
+			'blogger.com' => 'blogger',
+			'blogspot.com' => 'blogger',
+			'codepen.io' => 'codepen',
+			'deviantart.com' => 'deviantart',
+			'discord.gg' => 'discord',
+			'discordapp.com' => 'discord',
+			'digg.com' => 'digg',
+			'dribbble.com' => 'dribbble',
+			'dropbox.com' => 'dropbox',
+			'etsy.com' => 'etsy',
+			'eventbrite.com' => 'eventbrite',
+			'facebook.com' => 'facebook',
+			'flickr.com' => 'flickr',
+			'foursquare.com' => 'foursquare',
+			'ghost.org' => 'ghost',
+			'goodreads.com' => 'goodreads',
+			'google.com' => 'google',
+			'github.com' => 'github',
+			'instagram.com' => 'instagram',
+			'linkedin.com' => 'linkedin',
+			'mailto:' => 'mail',
+			'meetup.com' => 'meetup',
+			'medium.com' => 'medium',
+			'patreon.com' => 'patreon',
+			'pinterest.' => 'pinterest',
+			'getpocket.com' => 'pocket',
+			'ravelry.com' => 'ravelry',
+			'reddit.com' => 'reddit',
+			'skype.com' => 'skype',
+			'skype:' => 'skype',
+			'slideshare.net' => 'slideshare',
+			'snapchat.com' => 'snapchat',
+			'soundcloud.com' => 'soundcloud',
+			'spotify.com' => 'spotify',
+			'stackoverflow.com' => 'stackoverflow',
+			'stumbleupon.com' => 'stumbleupon',
+			'telegram.me' => 'telegram',
+			'tiktok.com' => 'tiktok',
+			'tumblr.com' => 'tumblr',
+			'twitch.tv' => 'twitch',
+			'twitter.com' => 'twitter',
+			'vimeo.com' => 'vimeo',
+			'vk.com' => 'vk',
+			'whatsapp.com' => 'whatsapp',
+			'woocommerce.com' => 'woocommerce',
+			'wordpress.org' => 'wordpress',
+			'wordpress.com' => 'wordpress',
+			'yelp.com' => 'yelp',
+			'xanga.com' => 'xanga',
+			'youtube.com' => 'youtube'
 		];
 
 		if ($tmp and count($tmp)) {
@@ -48,6 +108,24 @@ class SocialLinks extends Module {
 						'name' => $name,
 						'url' => $url
 					];
+				}
+
+				# Other social urls
+				if (substr($k, -4) === 'urls' and is_array($v) and !empty($v)) {
+					foreach($v as $url) {
+						$host = parse_url($url, PHP_URL_HOST);
+						$name = array_filter($urlMap, function($url) use ($host) {
+							return str_contains($url, $host) || str_contains($host, $url);
+						}, ARRAY_FILTER_USE_KEY);
+
+						$name = reset($name) ? reset($name) : $host;
+						$name = isset($nicenames[$name]) ? $nicenames[$name] : ucfirst(str_replace(['_', '-'], ' ', $name));
+
+						$links[] = (object) [
+							'name' => $name,
+							'url' => $url
+						];
+					}
 				}
 			}
 		}

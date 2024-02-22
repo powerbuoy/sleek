eec0758e877dd71d06f08738efa967a389ed1b5e
powerbuoy <powerbuoy@gmail.com>
2024-02-22 16:47:40 +0100

    started on media component

--- components/media/fields.php
+++ components/media/fields.php
@@ -1,12 +1,51 @@
-<?php return function ($args = []) {
+<?php return function ($moduleName, $args = []) {
+	$config = array_merge([
+		'module_name' => null,
+		'ratio' => true
+	], $args);
+
+	# Main media file
+	$fields = [
+		[
+			'name' => 'media',
+			'label' => __('Image or Video', 'sleek_admin'),
+			'type' => 'file',
+			'return_format' => 'id'
+		]
+	];
+
+	if ($config['ratio']) {
+		$fields = array_merge($fields, (include get_stylesheet_directory() . '/components/ratio.php')());
+	}
+
+	# Optional portrait media file
+	$condition = [[[
+		'field' => '{acf_key}_' . $moduleName . '_media_media', # NOTE: First media is the group
+		'operator' => '!=empty'
+	]]];
+
+	$fields[] = [
+		'name' => 'media_portrait',
+		'label' => __('Portrait Media', 'sleek_admin'),
+		'instructions' => __('Optionally select another image or video to be used in portrait mode.', 'sleek_admin'),
+		'type' => 'file',
+		'return_format' => 'id',
+		'conditional_logic' => $condition
+	];
+
+	if ($config['ratio']) {
+		$fields = array_merge($fields, (include get_stylesheet_directory() . '/components/ratio.php')([
+			'name' => 'ratio_portrait',
+			'conditional_logic' => $condition
+		]));
+	}
+
 	return [
 		[
 			'name' => 'media',
 			'label' => __('Media', 'sleek_admin'),
 			'type' => 'group',
-			'sub_fields' => [
-
-			]
+			'sub_fields' => $fields
 		]
 	];
 };
--- components/ratio.php
+++ components/ratio.php
@@ -1,12 +1,25 @@
 <?php return function ($args = []) {
-	return [
-		[
-			'name' => 'ratio',
-			'label' => __('Ratio', 'sleek_admin'),
-			'type' => 'select',
-			'choices' => [
-				'16-9' => '16:9'
-			]
+	$config = array_merge([
+		'name' => 'ratio',
+		'label' => __('Ratio', 'sleek_admin'),
+		'conditional_logic' => false,
+		'ratios' => [
+			'auto' => __('Auto', 'sleek_admin'),
+			'16-9' => '16:9'
 		]
+	], $args);
+
+	$field = [
+		'name' => $config['name'],
+		'label' => $config['label'],
+		'type' => 'select',
+		'choices' => $config['ratios'],
+		'default_value' => 'auto'
 	];
+
+	if ($config['conditional_logic']) {
+		$field['conditional_logic'] = $config['conditional_logic'];
+	}
+
+	return [$field];
 };

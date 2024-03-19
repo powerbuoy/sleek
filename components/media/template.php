127a8f91e41bbd4fd474579dd381b61f7db6cbac
powerbuoy <powerbuoy@gmail.com>
2024-03-20 00:36:00 +0100

    Enable link in media

--- components/media/template.php
+++ components/media/template.php
@@ -12,7 +12,8 @@
 			'disableremoteplayback',
 			'playsinline',
 			'loop'
-		]
+		],
+		'link' => null
 	], $args);
 
 	$media_id = null;
@@ -45,11 +46,38 @@
 			$media_portrait_classes[] = 'ratio--' . $args['media']['ratio_portrait'];
 		}
 	}
+
+	# Potential link
+	$link_url = '';
+	$link_target = '';
+	$link_start = '';
+	$link_end = '';
+
+	if (!empty($args['link'])) {
+		# Link to media
+		if ($args['link'] === true) {
+			$link_url = $media_url;
+			$link_target = 'target="_blank"';
+		}
+		# ACF link / array link
+		elseif (!empty($args['link']['url'])) {
+			$link_url = $args['link']['url'];
+			$link_target = !empty($args['link']['target']) ? 'target="' . $args['link']['target'] . '"' : '';
+		}
+		# String
+		else {
+			$link_url = $args['link'];
+		}
+
+		$link_start = '<a href="' . $link_url . '" ' . $link_target . '>';
+		$link_end = '</a>';
+	}
 ?>
 
 <?php if ($media_url) : ?>
 	<figure class="<?php echo $args['class'] ?>">
 		<div class="<?php echo implode(' ', $media_classes) ?>">
+			<?php echo $link_start ?>
 			<?php if (isset($media_meta['mime_type']) and strpos($media_meta['mime_type'], 'video') !== false) : ?>
 				<video
 					src="<?php echo $media_url ?>"
@@ -60,9 +88,11 @@
 			<?php else : ?>
 				<?php echo wp_get_attachment_image($media_id, $args['size'], false, ['loading' => $args['loading']]) ?>
 			<?php endif ?>
+			<?php echo $link_end ?>
 		</div>
 		<?php if ($media_portrait_url) : ?>
 			<div class="<?php echo implode(' ', $media_portrait_classes) ?>">
+				<?php echo $link_start ?>
 				<?php if (isset($media_portrait_meta['mime_type']) and strpos($media_portrait_meta['mime_type'], 'video') !== false) : ?>
 					<video
 						src="<?php echo $media_portrait_url ?>"
@@ -73,6 +103,7 @@
 				<?php else : ?>
 					<?php echo wp_get_attachment_image($media_portrait_id, $args['size_portrait'], false, ['loading' => $args['loading']]) ?>
 				<?php endif ?>
+				<?php echo $link_end ?>
 			</div>
 		<?php endif ?>
 	</figure>

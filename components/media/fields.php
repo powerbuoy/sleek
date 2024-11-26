87420fe36526187e800de2da717eb44912994a76
powerbuoy <powerbuoy@gmail.com>
2024-11-26 11:17:55 +0100

    Media embed and description

--- components/media/fields.php
+++ components/media/fields.php
@@ -1,9 +1,13 @@
 <?php return function ($moduleName, $args = []) {
 	$config = array_merge([
 		'ratio' => true,
+		'embed' => false,
+		'description' => true,
+		'required' => false,
 		'name' => 'media',
 		'label' => __('Media', 'sleek_admin'),
-		'additional_fields' => []
+		'additional_fields' => [],
+		'acf_key' => '{acf_key}_' # NOTE: This is useful for modules, but when used in a CPT's fields this should be empty
 	], $args);
 
 	# Work out all the media mime types
@@ -23,15 +27,24 @@
 			'name' => 'media',
 			'label' => __('Image or Video', 'sleek_admin'),
 			'type' => 'file',
+			'required' => $config['required'],
 			'return_format' => 'id',
 			'mime_types' => implode(',', $mediaExts) # NOTE: The property is called mime_types, but the expected value is a comma-separated list of extensions
 		]
 	];
 
+	if ($config['description']) {
+		$fields[] = [
+			'name' => 'description',
+			'label' => __('Media description', 'sleek_admin'),
+			'type' => 'text'
+		];
+	}
+
 	if ($config['ratio']) {
 		$fields = array_merge($fields, (include get_stylesheet_directory() . '/components/ratio.php')([
 			'conditional_logic' => [[[
-				'field' => '{acf_key}_' . $moduleName . '_' . $config['name'] . '_media',
+				'field' => $config['acf_key'] . $moduleName . '_' . $config['name'] . '_media',
 				'operator' => '!=empty'
 			]]]
 		]));
@@ -46,7 +59,7 @@
 		'return_format' => 'id',
 		'mime_types' => implode(',', $mediaExts),
 		'conditional_logic' => [[[
-			'field' => '{acf_key}_' . $moduleName . '_' . $config['name'] . '_media',
+			'field' => $config['acf_key'] . $moduleName . '_' . $config['name'] . '_media',
 			'operator' => '!=empty'
 		]]]
 	];
@@ -55,7 +68,7 @@
 		$fields = array_merge($fields, (include get_stylesheet_directory() . '/components/ratio.php')([
 			'name' => 'ratio_portrait',
 			'conditional_logic' => [[[
-				'field' => '{acf_key}_' . $moduleName . '_' . $config['name'] . '_media_portrait',
+				'field' => $config['acf_key'] . $moduleName . '_' . $config['name'] . '_media_portrait',
 				'operator' => '!=empty'
 			]]]
 		]));
@@ -65,12 +78,36 @@
 	$fields = array_merge($fields, $config['additional_fields']);
 
 	# Return as group
+	if ($config['embed']) {
+		$sub_fields = [
+			[
+				'name' => 'media_tab',
+				'label' => __('Media', 'sleek_admin'),
+				'type' => 'tab'
+			],
+			...$fields,
+			[
+				'name' => 'embed_tab',
+				'label' => __('Embed', 'sleek_admin'),
+				'type' => 'tab'
+			],
+			[
+				'name' => 'video_embed',
+				'label' => __('Video', 'sleek_admin'),
+				'type' => 'oembed'
+			],
+		];
+	}
+	else {
+		$sub_fields = $fields;
+	}
+
 	return [
 		[
 			'name' => $config['name'],
 			'label' => $config['label'],
 			'type' => 'group',
-			'sub_fields' => $fields
+			'sub_fields' => $sub_fields
 		]
 	];
 };
--- components/media/template.php
+++ components/media/template.php
@@ -23,61 +23,87 @@
 	$media_portrait_url = null;
 	$media_portrait_classes = [];
 
-	# Default media
-	if (!empty($args['media']['media'])) {
-		$media_id = $args['media']['media'];
-		$media_url = wp_get_attachment_url($args['media']['media']);
-		$media_meta = wp_get_attachment_metadata($args['media']['media']);
-
-		if (!empty($args['media']['ratio']) and $args['media']['ratio'] !== 'auto') {
-			$media_classes[] = 'ratio--' . $args['media']['ratio'];
-		}
+	# Video embed
+	if (!empty($args['media']['video_embed'])) {
+		echo '<figure class="media video">' . $args['media']['video_embed'] . '</figure>';
 	}
-
-	# Portrait
-	if (!empty($args['media']['media_portrait'])) {
-		$media_portrait_id = $args['media']['media_portrait'];
-		$media_portrait_url = wp_get_attachment_url($args['media']['media_portrait']);
-		$media_portrait_meta = wp_get_attachment_metadata($args['media']['media_portrait']);
-		$media_classes[] = 'portrait:hide';
-		$media_portrait_classes[] = 'portrait:show';
-
-		if (!empty($args['media']['ratio_portrait']) and $args['media']['ratio_portrait'] !== 'auto') {
-			$media_portrait_classes[] = 'ratio--' . $args['media']['ratio_portrait'];
-		}
-	}
-
-	# Potential link
-	$link_url = '';
-	$link_target = '';
-	$link_start = '';
-	$link_end = '';
-
-	if (!empty($args['link'])) {
-		# Link to media
-		if ($args['link'] === true) {
-			$link_url = $media_url;
-			$link_target = 'target="_blank"';
-		}
-		# ACF link / array link
-		elseif (!empty($args['link']['url'])) {
-			$link_url = $args['link']['url'];
-			$link_target = !empty($args['link']['target']) ? 'target="' . $args['link']['target'] . '"' : '';
-		}
-		# String
-		else {
-			$link_url = $args['link'];
+	# Media
+	else {
+		# Border radius?
+		if (!empty($args['media']['border_radius']) and $args['media']['border_radius'] !== 'none') {
+			$media_classes[] = 'border-radius--' . $args['media']['border_radius'];
+			$media_portrait_classes[] = 'border-radius--' . $args['media']['border_radius'];
 		}
 
-		$link_start = '<a href="' . $link_url . '" ' . $link_target . '>';
-		$link_end = '</a>';
+		# Default media
+		if (!empty($args['media']['media'])) {
+			$media_id = $args['media']['media'];
+			$media_url = wp_get_attachment_url($args['media']['media']);
+			$media_meta = wp_get_attachment_metadata($args['media']['media']);
+
+			if (!empty($args['media']['ratio'])) {
+				if ($args['media']['ratio'] !== 'auto') {
+					$media_classes[] = 'ratio--' . $args['media']['ratio'];
+				}
+				else {
+					$args['size'] = 'original_' . $args['size'];
+				}
+			}
+		}
+
+		# Portrait
+		if (!empty($args['media']['media_portrait'])) {
+			$media_portrait_id = $args['media']['media_portrait'];
+			$media_portrait_url = wp_get_attachment_url($args['media']['media_portrait']);
+			$media_portrait_meta = wp_get_attachment_metadata($args['media']['media_portrait']);
+			$media_classes[] = 'portrait:hide';
+			$media_portrait_classes[] = 'portrait:show';
+
+			if (!empty($args['media']['ratio_portrait'])) {
+				if ($args['media']['ratio_portrait'] !== 'auto') {
+					$media_portrait_classes[] = 'ratio--' . $args['media']['ratio_portrait'];
+				}
+				else {
+					$args['size_portrait'] = 'original_' . $args['size_portrait'];
+				}
+			}
+		}
+
+		# Potential link
+		$link_url = '';
+		$link_target = '';
+		$link_start = '';
+		$link_end = '';
+
+		if (!empty($args['link'])) {
+			# Link to media
+			if ($args['link'] === true) {
+				$link_url = $media_url;
+				$link_target = 'target="_blank"';
+			}
+			# ACF link / array link
+			elseif (!empty($args['link']['url'])) {
+				$link_url = $args['link']['url'];
+				$link_target = !empty($args['link']['target']) ? 'target="' . $args['link']['target'] . '"' : '';
+			}
+			# String
+			else {
+				$link_url = $args['link'];
+			}
+
+			$link_start = '<a href="' . $link_url . '" ' . $link_target . '>';
+			$link_end = '</a>';
+		}
 	}
 ?>
 
 <?php if ($media_url) : ?>
 	<figure class="<?php echo $args['class'] ?>">
+
 		<div class="<?php echo implode(' ', $media_classes) ?>">
+
 			<?php echo $link_start ?>
+
 			<?php if (isset($media_meta['mime_type']) and strpos($media_meta['mime_type'], 'video') !== false) : ?>
 				<video
 					src="<?php echo $media_url ?>"
@@ -88,11 +114,17 @@
 			<?php else : ?>
 				<?php echo wp_get_attachment_image($media_id, $args['size'], false, ['loading' => $args['loading']]) ?>
 			<?php endif ?>
+
 			<?php echo $link_end ?>
+
 		</div>
+
 		<?php if ($media_portrait_url) : ?>
+
 			<div class="<?php echo implode(' ', $media_portrait_classes) ?>">
+
 				<?php echo $link_start ?>
+
 				<?php if (isset($media_portrait_meta['mime_type']) and strpos($media_portrait_meta['mime_type'], 'video') !== false) : ?>
 					<video
 						src="<?php echo $media_portrait_url ?>"
@@ -103,8 +135,15 @@
 				<?php else : ?>
 					<?php echo wp_get_attachment_image($media_portrait_id, $args['size_portrait'], false, ['loading' => $args['loading']]) ?>
 				<?php endif ?>
+
 				<?php echo $link_end ?>
+
 			</div>
 		<?php endif ?>
+
+		<?php if (!empty($args['media']['description'])) : ?>
+			<figcaption><?php echo $args['media']['description'] ?></figcaption>
+		<?php endif ?>
+
 	</figure>
 <?php endif ?>
